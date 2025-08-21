609012461208abced5ad2bb7093caff5ce67a46f
Goldenshacal <goldenshacal@gmail.com>
2025-08-21 20:41:44 +0200

    better formating

--- .contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/2 - Argus/0 - Factions.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/2 - Argus/0 - Factions.lua
@@ -1,13 +1,17 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
-root(ROOTS.Zones, m(BROKEN_ISLES, m(ARGUS, bubbleDown({ ["timeline"] = { ADDED_7_3_0 } }, {
-	n(FACTIONS, {
-		faction(FACTION_ARGUSSIAN_REACH, {
-			ach(12076),	-- Protector of the Argussian Reach
-		}),
-		faction(FACTION_ARMY_OF_THE_LIGHT, {
-			ach(12081),	-- Officer of the Light
-		}),
+root(ROOTS.Zones, {
+	m(BROKEN_ISLES, {
+		m(ARGUS, bubbleDown({ ["timeline"] = { ADDED_7_3_0 } }, {
+			n(FACTIONS, {
+				faction(FACTION_ARGUSSIAN_REACH, {
+					ach(12076),	-- Protector of the Argussian Reach
+				}),
+				faction(FACTION_ARMY_OF_THE_LIGHT, {
+					ach(12081),	-- Officer of the Light
+				}),
+			}),
+		})),
 	}),
-}))));
+});
