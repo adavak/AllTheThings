2cf6ea65086d5df9fd3ecacd49a0d72b38bacd9f
Goldenshacal <goldenshacal@gmail.com>
2025-08-21 18:46:11 +0200

    added argus header to legion remix zones to fit with "retail"

--- .contrib/Parser/DATAS/07 - Events/Remix Legion/Outdoor Zones/Antoran Wastes.lua
+++ .contrib/Parser/DATAS/07 - Events/Remix Legion/Outdoor Zones/Antoran Wastes.lua
@@ -2,7 +2,7 @@
 --     W O R L D   E V E N T S   M O D U L E       --
 -----------------------------------------------------
 --[[root(ROOTS.WorldEvents, applyevent(EVENTS.REMIX_LEGION, n(REMIX_LEGION, bubbleDown({ ["timeline"] = { ADDED_11_2_5, REMOVED_LEGION_REMIX_END } }, {
-	m(ANTORAN_WASTES, {
+	m(ARGUS, m(ANTORAN_WASTES, {
 		["icon"] = 618796,
 		["groups"] = {
 			n(ACHIEVEMENTS, {
@@ -158,5 +158,5 @@
 				}),
 			}),
 		},
-	}),
+	})),
 }))));--]]
--- .contrib/Parser/DATAS/07 - Events/Remix Legion/Outdoor Zones/Eredath.lua
+++ .contrib/Parser/DATAS/07 - Events/Remix Legion/Outdoor Zones/Eredath.lua
@@ -2,7 +2,7 @@
 --     W O R L D   E V E N T S   M O D U L E       --
 -----------------------------------------------------
 --[[root(ROOTS.WorldEvents, applyevent(EVENTS.REMIX_LEGION, n(REMIX_LEGION, bubbleDown({ ["timeline"] = { ADDED_11_2_5, REMOVED_LEGION_REMIX_END } }, {
-	m(EREDATH, {
+	m(ARGUS, m(EREDATH, {
 		["icon"] = 618317,
 		["groups"] = {
 			n(ACHIEVEMENTS, {
@@ -149,5 +149,5 @@
 				}),
 			}),
 		},
-	}),
+	})),
 }))));--]]
