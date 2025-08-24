7be236ac06570d5feea5def474c2e70975161f21
Darkal <michalbert@seznam.cz>
2025-08-24 08:45:26 +0200

    Added back Explorations which are used by Achievement Criterias and are causing parser warnings when missing.

--- .contrib/Parser/DATAS/17 - NYI/NYI Explorations.lua
+++ .contrib/Parser/DATAS/17 - NYI/NYI Explorations.lua
@@ -3,4 +3,33 @@
 ---------------------------------------------------------------
 root(ROOTS.NeverImplemented, explorationHeader({
 	-- TODO: Add these whenever we are actually close to be done with sorting Explorations overall
+
+
+	--- These are used by Achievement Criterias ---
+	--- Remove them from here when added correctly ---
+	exploration(13745),	-- Mirewood Fen
+	exploration(13708),	-- Plain of Actualization
+	exploration(11526),	-- Zovaal's Cauldron
+	exploration(10987),	-- The Shrouded Asylum
+	exploration(10984),	-- Court of The Harvesters
+	exploration(11000),	-- Redelav District
+	exploration(11007),	-- Stalker's Lodge
+	exploration(11017),	-- Witherfall Ruin
+	exploration(13624),	-- Sanctuary of Guidance
+	exploration(12844),	-- Shimmerbough
+	exploration(11516),	-- Dusty Burrows
+	exploration(8729),	-- The Sliver
+	exploration(7089),	-- Tomb of Lights
+	exploration(5103),	-- The Scalding Chasm
+	exploration(5134),	-- Seabrush
+	exploration(3856),	-- Elrendar Crossing
+	exploration(2420),	-- Terror Wing Path
+	exploration(4882),	-- Marshal's Stand
+	exploration(4883),	-- Mossy Pile
+	exploration(5642),	-- Tahret Grounds
+	exploration(4933),	-- Krom'gar Fortress
+	exploration(4929),	-- Windshear Hold
+	exploration(1231),	-- Southridge Beach
+	exploration(4690),	-- Thunder Peak
+	exploration(4644),	-- Orendil's Retreat
 }));
