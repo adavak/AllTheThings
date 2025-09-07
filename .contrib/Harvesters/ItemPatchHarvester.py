f1e11732fdfbac2ff9500ecef56829bd529e8e8d
Dmitry Volodin <mr.molkree@gmail.com>
2025-09-07 18:22:06 +0400

    Dropped some old unused code for timeline harvesting from Wowhead

--- .contrib/Harvesters/ItemPatchHarvester.py
+++
@@ -1,6 +0,0 @@
-from PatchHarvester import harvest_things
-
-MAX_ID_PATH = "MaxItemID.txt"
-ITEM_DB_PATH = "item_timelines.txt"
-
-harvest_things("item", MAX_ID_PATH, ITEM_DB_PATH)
--- .contrib/Harvesters/QuestPatchHarvester.py
+++
@@ -1,6 +0,0 @@
-from PatchHarvester import harvest_things
-
-MAX_ID_PATH = "MaxQuestID.txt"
-QUEST_DB_PATH = "quest_timelines.txt"
-
-harvest_things("quest", MAX_ID_PATH, QUEST_DB_PATH)
