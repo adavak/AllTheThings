517109185f22a1aab25f801788640808da15c44e
Darkal <michalbert@seznam.cz>
2025-10-05 12:21:29 +0200

    Deduplicated all MoP timewalking dungeons.

--- .contrib/Parser/DATAS/21 - Holidays/Timewalking.lua
+++ .contrib/Parser/DATAS/21 - Holidays/Timewalking.lua
@@ -5039,40 +5039,52 @@ root(ROOTS.Holidays, n(TIMEWALKING_HEADER, applyevent(EVENTS.TIMEWALKING_MISTS_O
 			}),
 		}),
 		inst_tw(303, {	-- Gate of the Setting Sun
-			cr(56906, e(655, {	-- Saboteur Kip'tilak
-				i(144018),	-- Fallout-Filtering Hood
-				i(144102),	-- Pendant of Precise Timing
-				i(144100),	-- Saboteur's Stablizing Bracers
-				i(144134),	-- Grenadier's Belt
-				i(144135),	-- Pulled Grenade Pin
-			})),
-			cr(56589, e(675, {	-- Striker Ga'dok
-				i(143980),	-- Acid-Scarred Spaulders
-				i(144137),	-- Bomber's Precision Gloves
-				i(144095),	-- Impaler's Girdle
-				i(143983),	-- Airstream Treads
-				i(144136),	-- Vision of the Predator
-			})),
-			cr(56636, e(676, {	-- Commander Ri'mok
-				i(144098),	-- Mantid Trochanter
-				i(144019),	-- Swarmcall Helm
-				i(144138),	-- Ri'mok's Shattered Scale
-				i(143982),	-- Leggings of the Frenzy
-				i(144139),	-- Viscous Ring
-			})),
-			cr(56877, e(649, {	-- Raigonn
-				i(144101),	-- Carapace Breaker
-				i(144218),	-- Klatith, Fangs of the Swarm
-				i(144140),	-- Impervious Carapace
-				i(144104),	-- Shield of the Protectorate
-				i(143991),	-- Shoulders of Engulfing Winds
-				i(144141),	-- Drape of the Screeching Swarm
-				i(143955),	-- Swarmbringer Chestguards
-				i(144142),	-- Frenzyswarm Bracers
-				i(144105),	-- Hive Protector's Gauntlets
-				i(143977),	-- Wall-Breaker Legguards
-				i(143984),	-- Treads of Fixation
-			})),
+			e(655, {	-- Saboteur Kip'tilak
+				["crs"] = { 56906 },	-- Saboteur Kip'tilak
+				["sym"] = {{"select", "itemID",
+					144018,	-- Fallout-Filtering Hood
+					144102,	-- Pendant of Precise Timing
+					144100,	-- Saboteur's Stablizing Bracers
+					144134,	-- Grenadier's Belt
+					144135,	-- Pulled Grenade Pin
+				}},
+			}),
+			e(675, {	-- Striker Ga'dok
+				["crs"] = { 56589 },	-- Striker Ga'dok
+				["sym"] = {{"select", "itemID",
+					143980,	-- Acid-Scarred Spaulders
+					144137,	-- Bomber's Precision Gloves
+					144095,	-- Impaler's Girdle
+					143983,	-- Airstream Treads
+					144136,	-- Vision of the Predator
+				}},
+			}),
+			e(676, {	-- Commander Ri'mok
+				["crs"] = { 56636 },	-- Commander Ri'mok
+				["sym"] = {{"select", "itemID",
+					144098,	-- Mantid Trochanter
+					144019,	-- Swarmcall Helm
+					144138,	-- Ri'mok's Shattered Scale
+					143982,	-- Leggings of the Frenzy
+					144139,	-- Viscous Ring
+				}},
+			}),
+			e(649, {	-- Raigonn
+				["crs"] = { 56877 },	-- Raigonn
+				["sym"] = {{"select", "itemID",
+					144101,	-- Carapace Breaker
+					144218,	-- Klatith, Fangs of the Swarm
+					144140,	-- Impervious Carapace
+					144104,	-- Shield of the Protectorate
+					143991,	-- Shoulders of Engulfing Winds
+					144141,	-- Drape of the Screeching Swarm
+					143955,	-- Swarmbringer Chestguards
+					144142,	-- Frenzyswarm Bracers
+					144105,	-- Hive Protector's Gauntlets
+					143977,	-- Wall-Breaker Legguards
+					143984,	-- Treads of Fixation
+				}},
+			}),
 		}),
 		inst_tw(321, {	-- Mogu'shan Palace
 			e(708, {	-- Trial of the King (Ming the Cunning; 61442 Kuai the Brute, 61445 Haiyan the Unstoppable)
@@ -5081,39 +5093,39 @@ root(ROOTS.Holidays, n(TIMEWALKING_HEADER, applyevent(EVENTS.TIMEWALKING_MISTS_O
 					61442,	-- Kuai the Brute
 					61444,	-- Ming the Cunning
 				},
-				["groups"] = {
-					i(144020),	-- Crest of the Clan Lords
-					i(144021),	-- Meteoric Greathelm
-					i(144144),	-- Whirling Dervish Choker
-					i(144145),	-- Conflagrating Gloves
-					i(144143),	-- Hurricane Belt
-				},
+				["sym"] = {{"select", "itemID",
+					144020,	-- Crest of the Clan Lords
+					144021,	-- Meteoric Greathelm
+					144144,	-- Whirling Dervish Choker
+					144145,	-- Conflagrating Gloves
+					144143,	-- Hurricane Belt
+				}},
 			}),
 			e(690, {	-- Gekkan
 				["crs"] = { 61243 },	-- Gekkan
-				["groups"] = {
-					i(144148),	-- Claws of Gekkan
-					i(144147),	-- Cloak of Cleansing Flames
-					i(144149),	-- Hexxer's Lethargic Gloves
-					i(143992),	-- Glintrok Sollerets
-					i(144146),	-- Iron Protector Talisman
-				},
+				["sym"] = {{"select", "itemID",
+					144148,	-- Claws of Gekkan
+					144147,	-- Cloak of Cleansing Flames
+					144149,	-- Hexxer's Lethargic Gloves
+					143992,	-- Glintrok Sollerets
+					144146,	-- Iron Protector Talisman
+				}},
 			}),
 			e(698, {	-- Xin the Weaponmaster
 				["crs"] = { 61398 },	-- Xin the Weaponmaster
-				["groups"] = {
-					i(144150),	-- Ghostheart
-					i(144214),	-- Mogu'Dar, Blade of the Thousand Slaves
-					i(144154),	-- Firescribe Dagger
-					i(144155),	-- Firescribe Dagger
-					i(143995),	-- Regal Silk Shoulderpads
-					i(143956),	-- Mind's Eye Breastplate
-					i(144153),	-- Groundshaker Bracer
-					i(144151),	-- Axebreaker Gauntlets
-					i(143993),	-- Boots of Plummeting Death
-					i(143994),	-- Soulbinder Treads
-					i(144152),	-- Blade Trap Signet
-				},
+				["sym"] = {{"select", "itemID",
+					144150,	-- Ghostheart
+					144214,	-- Mogu'Dar, Blade of the Thousand Slaves
+					144154,	-- Firescribe Dagger
+					144155,	-- Firescribe Dagger
+					143995,	-- Regal Silk Shoulderpads
+					143956,	-- Mind's Eye Breastplate
+					144153,	-- Groundshaker Bracer
+					144151,	-- Axebreaker Gauntlets
+					143993,	-- Boots of Plummeting Death
+					143994,	-- Soulbinder Treads
+					144152,	-- Blade Trap Signet
+				}},
 			}),
 		}),
 		inst_tw(246, bubbleDownSelf({ ["timeline"] = { ADDED_9_1_5 }},{	-- Scholomance
@@ -5122,201 +5134,246 @@ root(ROOTS.Holidays, n(TIMEWALKING_HEADER, applyevent(EVENTS.TIMEWALKING_MISTS_O
 					58633,	-- Instructor Chillheart
 					58664,	-- Instructor Chillheart's Phylactery
 				},
-				["groups"] = {
-					i(144201),	-- Gravetouch Greatsword
-					i(144180),	-- Anarchist's Pendant
-					i(143967),	-- Breastplate of Wracking Souls
-					i(144200),	-- Shadow Puppet Bracers
-					i(144202),	-- Icewrath Belt
-				},
+				["sym"] = {{"select", "itemID",
+					144201,	-- Gravetouch Greatsword
+					144180,	-- Anarchist's Pendant
+					143967,	-- Breastplate of Wracking Souls
+					144200,	-- Shadow Puppet Bracers
+					144202,	-- Icewrath Belt
+				}},
 			}),
 			e(663,   {	-- Jandice Barov
 				["crs"] = { 59184 },	-- Jandice Barov
-				["groups"] = {
-					i(144207),	-- Metanoia Shield
-					i(144029),	-- Barovian Ritual Hood
-					i(144208),	-- Phantasmal Drape
-					i(144012),	-- Ghostwoven Legguards
-					i(144013),	-- Wraithplate Treads
-				},
+				["sym"] = {{"select", "itemID",
+					144207,	-- Metanoia Shield
+					144029,	-- Barovian Ritual Hood
+					144208,	-- Phantasmal Drape
+					144012,	-- Ghostwoven Legguards
+					144013,	-- Wraithplate Treads
+				}},
 			}),
 			e(665,   {	-- Rattlegore
 				["crs"] = { 59153 },	-- Rattlegore
-				["groups"] = {
-					i(144011),	-- Bone Golem Boots
-					i(144204),	-- Deadwalker Bracers
-					i(144203),	-- Goresoaked Headreaper
-					i(144205),	-- Necromantic Wand
-					i(144206),	-- Rattling Gloves
-					i(144015),	-- Vigorsteel Spaulders
-				},
+				["sym"] = {{"select", "itemID",
+					144011,	-- Bone Golem Boots
+					144204,	-- Deadwalker Bracers
+					144203,	-- Goresoaked Headreaper
+					144205,	-- Necromantic Wand
+					144206,	-- Rattling Gloves
+					144015,	-- Vigorsteel Spaulders
+				}},
 			}),
 			n(59369, {	-- Doctor Theolen Krastinov
 				["description"] = "This is a Rare Creature and is not always present.",
-				["groups"] = {
-					i(88566),	-- Krastinov's Bag of Horrors (TOY!)
-				},
+				["sym"] = {{"select", "itemID",
+					88566,	-- Krastinov's Bag of Horrors (TOY!)
+				}},
 			}),
 			e(666,   {	-- Lilian Voss
 				["crs"] = { 58722 },	-- Lilian Voss
-				["groups"] = {
-					i(144030),	-- Soulburner Crown
-					i(144181),	-- Necklace of the Dark Blaze
-					i(143968),	-- Shivbreaker Vest
-					i(144209),	-- Dark Blaze Gauntlets
-					i(144014),	-- Leggings of Unleashed Anguish
-				},
+				["sym"] = {{"select", "itemID",
+					144030,	-- Soulburner Crown
+					144181,	-- Necklace of the Dark Blaze
+					143968,	-- Shivbreaker Vest
+					144209,	-- Dark Blaze Gauntlets
+					144014,	-- Leggings of Unleashed Anguish
+				}},
 			}),
 			e(684, {	-- Darkmaster Gandling
 				["crs"] = { 59080 },	-- Darkmaster Gandling
-				["groups"] = {
-					i(144212),	-- Gloves of Explosive Pain
-					i(144211),	-- Headmaster's Will
-					i(144213),	-- Incineration Belt
-					i(144161),	-- Lessons of the Darkmaster
-					i(144159),	-- Price of Progress
-					i(144160),	-- Searing Words
-					i(144016),	-- Shoulderguards of Painful Lessons
-					i(144210),	-- Tombstone Gauntlets
-					i(144015),	-- Vigorsteel Spaulders
-				},
+				["sym"] = {{"select", "itemID",
+					144212,	-- Gloves of Explosive Pain
+					144211,	-- Headmaster's Will
+					144213,	-- Incineration Belt
+					144161,	-- Lessons of the Darkmaster
+					144159,	-- Price of Progress
+					144160,	-- Searing Words
+					144016,	-- Shoulderguards of Painful Lessons
+					144210,	-- Tombstone Gauntlets
+					144015,	-- Vigorsteel Spaulders
+				}},
 			}),
 		})),
 		inst_tw(312, {	-- Shado-Pan Monastery
-			cr(56747, e(673, {	-- Gu Cloudstrike
-				i(144127),	-- Cloudstrike Pendant
-				i(143961),	-- Azure Serpent Chestguard
-				i(144126),	-- Star Summoner Bracers
-				i(144096),	-- Sparkbreath Girdle
-				i(143978),	-- Leggings of the Charging Soul
-			})),
-			cr(56541, e(657, {	-- Master Snowdrift
-				i(144110),	-- Snowdrift's Bladed Staff
-				i(144106),	-- Gauntlets of Resolute Fury
-				i(144097),	-- Quivering Heart Girdle
-				i(144129),	-- Eye of the Tornado
-				i(144128),	-- Heart of Fire
-			})),
-			cr(56719, e(685, {	-- Sha of Violence
-				i(144099),	-- Crescent of Ichor
-				i(144130),	-- Necklace of Disorientation
-				i(144131),	-- Bladed Smoke Bracers
-				i(144107),	-- Gloves of Enraged Slaughter
-				i(143985),	-- Spike-Soled Stompers
-			})),
-			cr(56884, e(686, {	-- Taran Zhu
-				i(144109),	-- Warmace of Taran Zhu
-				i(144215),	-- Ka'eng, Breath of the Shadow
-				i(144103),	-- Shield of Blind Hate
-				i(143981),	-- Shadowspine Shoulderguards
-				i(143962),	-- Hateshatter Chestplate
-				i(143990),	-- Robes of Fevered Dreams
-				i(144108),	-- Mindbender Plate Gloves
-				i(143979),	-- Darkbinder Leggings
-				i(143986),	-- Blastwalker Footguards
-				i(144133),	-- Ring of Malice
-				i(144132),	-- Seal of Hateful Meditation
-			})),
+			e(673, {	-- Gu Cloudstrike
+				["crs"] = { 56747 },	-- Gu Cloudstrike
+				["sym"] = {{"select", "itemID",
+					144127,	-- Cloudstrike Pendant
+					143961,	-- Azure Serpent Chestguard
+					144126,	-- Star Summoner Bracers
+					144096,	-- Sparkbreath Girdle
+					143978,	-- Leggings of the Charging Soul
+				}},
+			}),
+			e(657, {	-- Master Snowdrift
+				["crs"] = { 56541 },	-- Master Snowdrift
+				["sym"] = {{"select", "itemID",
+					144110,	-- Snowdrift's Bladed Staff
+					144106,	-- Gauntlets of Resolute Fury
+					144097,	-- Quivering Heart Girdle
+					144129,	-- Eye of the Tornado
+					144128,	-- Heart of Fire
+				}},
+			}),
+			e(685, {	-- Sha of Violence
+				["crs"] = { 56719 },	-- Sha of Violence
+				["sym"] = {{"select", "itemID",
+					144099,	-- Crescent of Ichor
+					144130,	-- Necklace of Disorientation
+					144131,	-- Bladed Smoke Bracers
+					144107,	-- Gloves of Enraged Slaughter
+					143985,	-- Spike-Soled Stompers
+				}},
+			}),
+			e(686, {	-- Taran Zhu
+				["crs"] = { 56884 },	-- Taran Zhu
+				["sym"] = {{"select", "itemID",
+					144109,	-- Warmace of Taran Zhu
+					144215,	-- Ka'eng, Breath of the Shadow
+					144103,	-- Shield of Blind Hate
+					143981,	-- Shadowspine Shoulderguards
+					143962,	-- Hateshatter Chestplate
+					143990,	-- Robes of Fevered Dreams
+					144108,	-- Mindbender Plate Gloves
+					143979,	-- Darkbinder Leggings
+					143986,	-- Blastwalker Footguards
+					144133,	-- Ring of Malice
+					144132,	-- Seal of Hateful Meditation
+				}},
+			}),
 		}),
 		inst_tw(324, bubbleDownSelf({ ["timeline"] = { ADDED_7_1_5, REMOVED_9_1_5 }},{	-- Siege of Niuzao Temple
-			cr(61567, e(693, {	-- Vizier Jin'bak
-				i(144022),	-- Hood of Viridian Residue
-				i(144162),	-- Engraved Amber Pendant
-				i(144163),	-- Girdle of Soothing Detonation
-				i(143996),	-- Sap-Encrusted Legplates
-				i(144156),	-- Flashfrozen Resin Globule
-			})),
-			cr(61634, e(738, {	-- Commander Vo'jak
-				i(144164),	-- Siege-Captain's Scimitar
-				i(144023),	-- Sightfinder Helm
-				i(143963),	-- Chestwrap of Arcing Flame
-				i(144165),	-- Bombardment Bracers
-				i(144166),	-- Archer's Precision Grips
-			})),
-			cr(61485, e(692, {	-- General Pa'valak
-				i(144167),	-- Tempestuous Longbow
-				i(144170),	-- Aerial Bombardment Cloak
-				i(144168),	-- Siegeworn Bracers
-				i(144169),	-- Breezebinder Handwraps
-				i(144157),	-- Vial of Ichorous Blood
-			})),
-			cr(62205, e(727, {	-- Wing Leader Ner'onok
-				i(144172),	-- Gustwalker Staff
-				i(144171),	-- Ner'onok's Razor Katar
-				i(144219),	-- Tolakesh, Horn of the Black Ox
-				i(144025),	-- Breezeswept Hood
-				i(144024),	-- Windblast Helm
-				i(143998),	-- Spaulders of Immovable Stone
-				i(143999),	-- Whisperwind Spaulders
-				i(143964),	-- Galedodger Chestguard
-				i(144173),	-- Belt of Totemic Binding
-				i(144000),	-- Airbender Sandals
-				i(143997),	-- Anchoring Sabatons
-			})),
+			e(693, {	-- Vizier Jin'bak
+				["crs"] = { 61567 },	-- Vizier Jin'bak
+				["sym"] = {{"select", "itemID",
+					144022,	-- Hood of Viridian Residue
+					144162,	-- Engraved Amber Pendant
+					144163,	-- Girdle of Soothing Detonation
+					143996,	-- Sap-Encrusted Legplates
+					144156,	-- Flashfrozen Resin Globule
+				}},
+			}),
+			e(738, {	-- Commander Vo'jak
+				["crs"] = { 61634 },	-- Commander Vo'jak
+				["sym"] = {{"select", "itemID",
+					144164,	-- Siege-Captain's Scimitar
+					144023,	-- Sightfinder Helm
+					143963,	-- Chestwrap of Arcing Flame
+					144165,	-- Bombardment Bracers
+					144166,	-- Archer's Precision Grips
+				}},
+			}),
+			e(692, {	-- General Pa'valak
+				["crs"] = { 61485 },	-- General Pa'valak
+				["sym"] = {{"select", "itemID",
+					144167,	-- Tempestuous Longbow
+					144170,	-- Aerial Bombardment Cloak
+					144168,	-- Siegeworn Bracers
+					144169,	-- Breezebinder Handwraps
+					144157,	-- Vial of Ichorous Blood
+				}},
+			}),
+			e(727, {	-- Wing Leader Ner'onok
+				["crs"] = { 62205 },	-- Wing Leader Ner'onok
+				["sym"] = {{"select", "itemID",
+					144172,	-- Gustwalker Staff
+					144171,	-- Ner'onok's Razor Katar
+					144219,	-- Tolakesh, Horn of the Black Ox
+					144025,	-- Breezeswept Hood
+					144024,	-- Windblast Helm
+					143998,	-- Spaulders of Immovable Stone
+					143999,	-- Whisperwind Spaulders
+					143964,	-- Galedodger Chestguard
+					144173,	-- Belt of Totemic Binding
+					144000,	-- Airbender Sandals
+					143997,	-- Anchoring Sabatons
+				}},
+			}),
 		})),
 		inst_tw(302, {	-- Stormstout Brewery
-			cr(56637, e(668, {	-- Ook-Ook
-				i(144084),	-- Ook's Hozen Slicer
-				i(143957),	-- Nimbletoe Chestguard
-				i(144087),	-- Bracers of Displaced Air
-				i(143989),	-- Barreldodger Boots
-				i(144119),	-- Empty Fruit Barrel
-			})),
-			cr(56717, e(669, {	-- Hoptallus
-				i(144092),	-- Bottle of Potent Potables
-				i(144121),	-- Cloak of Hidden Flasks
-				i(144088),	-- Bubble-Breaker Bracers
-				i(144120),	-- Belt of Brazen Inebriation
-				i(143975),	-- Hopping Mad Leggings
-			})),
-			cr(59479, e(670, {	-- Yan-Zhu the Uncasked
-				i(144124),	-- Wort Stirring Rod
-				i(144085),	-- Gao's Keg Tapper
-				i(144089),	-- Yan-Zhu's Pressure Valve
-				i(143969),	-- Fizzy Spaulders
-				i(143958),	-- Uncasked Chestguard
-				i(144082),	-- Fermenting Belt
-				i(143976),	-- Sudsy Legplates
-				i(144125),	-- Alemental Seal
-				i(144123),	-- Lime-Rimmed Signet
-				i(144122),	-- Carbonic Carbuncle
-			})),
+			e(668, {	-- Ook-Ook
+				["crs"] = { 56637 },	-- Ook-Ook
+				["sym"] = {{"select", "itemID",
+					144084,	-- Ook's Hozen Slicer
+					143957,	-- Nimbletoe Chestguard
+					144087,	-- Bracers of Displaced Air
+					143989,	-- Barreldodger Boots
+					144119,	-- Empty Fruit Barrel
+				}},
+			}),
+			e(669, {	-- Hoptallus
+				["crs"] = { 56717 },	-- Hoptallus
+				["sym"] = {{"select", "itemID",
+					144092,	-- Bottle of Potent Potables
+					144121,	-- Cloak of Hidden Flasks
+					144088,	-- Bubble-Breaker Bracers
+					144120,	-- Belt of Brazen Inebriation
+					143975,	-- Hopping Mad Leggings
+				}},
+			}),
+			e(670, {	-- Yan-Zhu the Uncasked
+				["crs"] = { 59479 },	-- Yan-Zhu the Uncasked
+				["sym"] = {{"select", "itemID",
+					144124,	-- Wort Stirring Rod
+					144085,	-- Gao's Keg Tapper
+					144089,	-- Yan-Zhu's Pressure Valve
+					143969,	-- Fizzy Spaulders
+					143958,	-- Uncasked Chestguard
+					144082,	-- Fermenting Belt
+					143976,	-- Sudsy Legplates
+					144125,	-- Alemental Seal
+					144123,	-- Lime-Rimmed Signet
+					144122,	-- Carbonic Carbuncle
+				}},
+			}),
 		}),
 		inst_tw(313, {	-- Temple of the Jade Serpent
-			cr(56448, e(672, {	-- Wise Mari
-				i(144017),	-- Waterburst Helm
-				i(144111),	-- Wind-Soaked
-				i(143960),	-- Riverbed Chestguard
-				i(143973),	-- Treads of Corrupted Water
-				i(144112),	-- Crystallized Droplet
-			})),
-			cr(56843, e(664, {	-- Lorewalker Stonestep
-				i(144081),	-- Girdle of Endemic Anger
-				i(144083),	-- Sunheart Waistband
-				i(143987),	-- Leggings of Whispered Dreams
-				i(143974),	-- Stonestep Boots
-				i(144113),	-- Windswept Pages
-			})),
-			cr(56732, e(658, {	-- Liu Flameheart
-				i(144090),	-- Firebelcher Hand Cannon
-				i(143971),	-- Serpentstrike Shoulderpads
-				i(144094),	-- Cape of Entaglement
-				i(143988),	-- Flameheart Sandals
-				i(144114),	-- Signet of Dancing Jade
-			})),
-			cr(56439, e(335, {	-- Sha of Doubt
-				i(144093),	-- Staff of Trembling Will
-				i(144086),	-- Dubious Handaxe
-				i(144216),	-- Je'lyu, Spirit of the Serpent
-				i(144117),	-- Mindbreaker Pendant
-				i(143972),	-- Doubtridden Shoulderguards
-				i(143970),	-- Neverdare Shoulders
-				i(144115),	-- Cloak of Failing Will
-				i(143959),	-- Chestguard of Despair
-				i(144091),	-- Hopecrusher Gauntlets
-				i(144118),	-- Paralyzing Gloves
-				i(144116),	-- Binding of Broken Dreams
-			})),
+			e(672, {	-- Wise Mari
+				["crs"] = { 56448 },	-- Wise Mari
+				["sym"] = {{"select", "itemID",
+					144017,	-- Waterburst Helm
+					144111,	-- Wind-Soaked
+					143960,	-- Riverbed Chestguard
+					143973,	-- Treads of Corrupted Water
+					144112,	-- Crystallized Droplet
+				}},
+			}),
+			e(664, {	-- Lorewalker Stonestep
+				["crs"] = { 56843 },	-- Lorewalker Stonestep
+				["sym"] = {{"select", "itemID",
+					144081,	-- Girdle of Endemic Anger
+					144083,	-- Sunheart Waistband
+					143987,	-- Leggings of Whispered Dreams
+					143974,	-- Stonestep Boots
+					144113,	-- Windswept Pages
+				}},
+			}),
+			e(658, {	-- Liu Flameheart
+				["crs"] = { 56732 },	-- Liu Flameheart
+				["sym"] = {{"select", "itemID",
+					144090,	-- Firebelcher Hand Cannon
+					143971,	-- Serpentstrike Shoulderpads
+					144094,	-- Cape of Entaglement
+					143988,	-- Flameheart Sandals
+					144114,	-- Signet of Dancing Jade
+				}},
+			}),
+			e(335, {	-- Sha of Doubt
+				["crs"] = { 56439 },	-- Sha of Doubt
+				["sym"] = {{"select", "itemID",
+					144093,	-- Staff of Trembling Will
+					144086,	-- Dubious Handaxe
+					144216,	-- Je'lyu, Spirit of the Serpent
+					144117,	-- Mindbreaker Pendant
+					143972,	-- Doubtridden Shoulderguards
+					143970,	-- Neverdare Shoulders
+					144115,	-- Cloak of Failing Will
+					143959,	-- Chestguard of Despair
+					144091,	-- Hopecrusher Gauntlets
+					144118,	-- Paralyzing Gloves
+					144116,	-- Binding of Broken Dreams
+				}},
+			}),
 		}),
 	})),
 })));
