a1d4ba6162573984de7893d649aa067fe5a6c6b5
Wouter <lostitz0r@gmail.com>
2025-10-07 13:32:51 +0200

    [DB] Point to the right Discord channel for users to report errors in Classic versions of the game

--- .contrib/Parser/lib/Strings/Classes/Transmog/SOURCE_ID_MISSING.lua
+++ .contrib/Parser/lib/Strings/Classes/Transmog/SOURCE_ID_MISSING.lua
@@ -1,18 +1,24 @@
+-- #if ANYCLASSIC
+local discordErrorChannel = '#classic-errors';
+-- #else
+local discordErrorChannel = '#retail-errors';
+-- #endif
+
 createLocalizationString({
-	readable = "Please report this Item and where it was acquired to the ATT Discord in #retail-errors!",
+	readable = "Please report this Item and where it was acquired to the ATT Discord in " .. discordErrorChannel .. "!",
 	constant = "SOURCE_ID_MISSING",
 	export = true,
 	text = {
-		en = "Please report this Item and where it was acquired to the ATT Discord in #retail-errors!",
-		de = "Bitte meldet diesen Gegenstand und wo er erhalten wurde an das ATT Discord in #retail-errors!",
-		es = "Por favor, reporta este objeto y dónde fue obtenido al Discord de ATT en #retail-errors!",
-		mx = "Por favor, reporta este objeto y dónde fue obtenido al Discord de ATT en #retail-errors!",
-		fr = "Veuillez signaler cet objet et le lieu où il a été acquis sur le Discord d’ATT, dans le salon #retail-errors !",
+		en = "Please report this Item and where it was acquired to the ATT Discord in " .. discordErrorChannel .. "!",
+		de = "Bitte meldet diesen Gegenstand und wo er erhalten wurde an das ATT Discord in " .. discordErrorChannel .. "!",
+		es = "Por favor, reporta este objeto y dónde fue obtenido al Discord de ATT en " .. discordErrorChannel .. "!",
+		mx = "Por favor, reporta este objeto y dónde fue obtenido al Discord de ATT en " .. discordErrorChannel .. "!",
+		fr = "Veuillez signaler cet objet et le lieu où il a été acquis sur le Discord d’ATT, dans le salon " .. discordErrorChannel .. " !",
 		-- TODO: it = "",
 		-- TODO: pt = "",
-		ru = "Пожалуйста, сообщите в канале #retail-errors на нашем сервере Discord, где Вы нашли эту вещь!",
+		ru = "Пожалуйста, сообщите в канале " .. discordErrorChannel .. " на нашем сервере Discord, где Вы нашли эту вещь!",
 		-- TODO: ko = "",
-		cn = "请在 ATT Discord 的 #retail-errors 中报告此物品及其获取地点！",
-		tw = "請在 ATT Discord 的 #retail-errors 中回報此物品及其獲得地點！",
+		cn = "请在 ATT Discord 的 " .. discordErrorChannel .. " 中报告此物品及其获取地点！",
+		tw = "請在 ATT Discord 的 " .. discordErrorChannel .. " 中回報此物品及其獲得地點！",
 	},
 })
--- .contrib/Parser/lib/Strings/base/PLEASE_REPORT_MESSAGE.lua
+++ .contrib/Parser/lib/Strings/base/PLEASE_REPORT_MESSAGE.lua
@@ -1,18 +1,24 @@
+-- #if ANYCLASSIC
+local discordErrorChannel = '#classic-errors';
+-- #else
+local discordErrorChannel = '#retail-errors';
+-- #endif
+
 createLocalizationString({
-	readable = "Please report this to the ATT Discord in #retail-errors! Thanks!",
+	readable = "Please report this to the ATT Discord in " .. discordErrorChannel .. "! Thanks!",
 	constant = "PLEASE_REPORT_MESSAGE",
 	export = true,
 	text = {
-		en = "Please report this to the ATT Discord in #retail-errors! Thanks!",
-		de = "Bitte melden Sie den Fehler an das ATT Discord in #retail-errors! Danke!",
-		es = "¡Por favor, reporte esto al Discord de ATT en #retail-errors! ¡gracias!",
-		mx = "¡Por favor, reporta esto al Discord de ATT en #retail-errors! ¡gracias!",
-		fr = "Merci de signaler sur le serveur Discord d’ATT dans le canal #retail-errors ! Merci !",
+		en = "Please report this to the ATT Discord in " .. discordErrorChannel .. "! Thanks!",
+		de = "Bitte melden Sie den Fehler an das ATT Discord in " .. discordErrorChannel .. "! Danke!",
+		es = "¡Por favor, reporte esto al Discord de ATT en " .. discordErrorChannel .. "! ¡gracias!",
+		mx = "¡Por favor, reporta esto al Discord de ATT en " .. discordErrorChannel .. "! ¡gracias!",
+		fr = "Merci de signaler sur le serveur Discord d’ATT dans le canal " .. discordErrorChannel .. " ! Merci !",
 		-- TODO: it = "",
-		pt = "Por favor reporte isso no Discord do ATT em #retail-errors! Obrigado!",
-		ru = "Пожалуйста, сообщите об этом на Discord-сервере ATT в канале #retail-errors! Спасибо!",
+		pt = "Por favor reporte isso no Discord do ATT em " .. discordErrorChannel .. "! Obrigado!",
+		ru = "Пожалуйста, сообщите об этом на Discord-сервере ATT в канале " .. discordErrorChannel .. "! Спасибо!",
 		-- TODO: ko = "",
-		cn = "请把错误报告给 ATT Discord 的 #retail-errors！谢谢！",
-		tw = "請把錯誤回報給 ATT Discord 的 #retail-errors！謝謝！",
+		cn = "请把错误报告给 ATT Discord 的 " .. discordErrorChannel .. "！谢谢！",
+		tw = "請把錯誤回報給 ATT Discord 的 " .. discordErrorChannel .. "！謝謝！",
 	},
 })
