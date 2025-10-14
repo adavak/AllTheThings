50ef73adc25d6be51ff483667b4b4f2afd31ea7a
ImUnicke <ImUnicke+github@gmail.com>
2025-10-14 13:52:41 -0600

    [Parser] Added new Concurrent types to handle additional scenarios * ConcurrentDataList - designed to support a sortable set of Data objects * ConcurrentHashSet - designed to support typical HashSet implementation in a concurrent-safe context

---
+++ .contrib/Source Code/Parser/Framework/ConcurrentDataList.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using static ATT.Export;
+using Data = System.Collections.Generic.IDictionary<string, object>;
+
+namespace ATT
+{
+    public class ConcurrentDataList : IEnumerable<Data>
+    {
+        private readonly List<Data> _list = new List<Data>();
+        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+        private readonly IComparer<Data> _comparer = new DataComparer();
+        private bool _sorted;
+
+        public void Add(Data value)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _list.Add(value);
+                _sorted = false;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        public IEnumerator<Data> GetEnumerator() => GetEnumeratorAfterSort();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumeratorAfterSort();
+
+        IEnumerator<Data> IEnumerable<Data>.GetEnumerator() => GetEnumeratorAfterSort();
+
+        public Data this[int index]
+        {
+            get
+            {
+                _lock.EnterReadLock();
+                try
+                {
+                    return _list[index];
+                }
+                finally
+                {
+                    _lock.ExitReadLock();
+                }
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                _lock.EnterReadLock();
+                try
+                {
+                    return _list.Count;
+                }
+                finally
+                {
+                    _lock.ExitReadLock();
+                }
+            }
+        }
+
+        private IEnumerator<Data> GetEnumeratorAfterSort()
+        {
+            _lock.EnterWriteLock();
+            if (!_sorted)
+                Sort();
+            _lock.ExitWriteLock();
+
+            return _list.GetEnumerator();
+        }
+
+        private void Sort()
+        {
+            _list.Sort(_comparer);
+            _sorted = true;
+        }
+
+        private class DataComparer : IComparer<Data>
+        {
+            public int Compare(Data x, Data y)
+            {
+                if (ReferenceEquals(x, y)) return 0;
+                if (x is null) return -1;
+                if (y is null) return 1;
+
+                ObjectData.TryGetMostSignificantObjectType(x, out ObjectData xo, out object akey);
+                ObjectData.TryGetMostSignificantObjectType(y, out ObjectData yo, out object bkey);
+
+                akey = ObjectData.GetAdjustedTypeKeyValue(xo, x);
+                bkey = ObjectData.GetAdjustedTypeKeyValue(yo, y);
+
+                if (akey is decimal adec && bkey is decimal bdec)
+                    return adec.CompareTo(bdec);
+
+                return Comparer<object>.Default.Compare(akey, bkey);
+            }
+        }
+    }
+}
---
+++ .contrib/Source Code/Parser/Framework/ConcurrentHashSet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATT
+{
+    public class ConcurrentHashSet<T> : IEnumerable<T>
+    {
+        private readonly ConcurrentDictionary<T, byte> _dict = new ConcurrentDictionary<T, byte>();
+
+        public bool Add(T item) => _dict.TryAdd(item, 0);
+        public bool Contains(T item) => _dict.ContainsKey(item);
+        public bool Remove(T item) => _dict.TryRemove(item, out _);
+
+        public IEnumerator<T> GetEnumerator() => _dict.Keys.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => _dict.Keys.GetEnumerator();
+    }
+}
