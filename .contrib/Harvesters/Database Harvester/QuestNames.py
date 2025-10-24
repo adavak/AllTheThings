99f03c973a374d2cf4f1e169f04fff48d5fe5953
gbrage <gbrage@kth.se>
2025-10-24 21:50:21 +0200

    Quest Names Harvest

--- .contrib/Harvesters/Database Harvester/QuestNames.py
+++ .contrib/Harvesters/Database Harvester/QuestNames.py
@@ -102,24 +102,23 @@ def get_available_expansions(patch: str) -> dict[str, str]:
             "Retail": "",
         }
         print("WoD-DF :", patch)
-    elif version.parse(patch) < version.parse("11.2.0.99999"):
+    elif version.parse(patch) < version.parse("11.2.5.99999"):
         expansion_dict = {
             "Retail": "",
             "PTR": "ptr",
             "PTR2": "ptr-2",
             #"BETA": "beta",
         }
-        print("11.0.0-11.2.0 :", patch)
-    elif version.parse(patch) < version.parse("11.2.5.99999"):
+        print("11.0.0-11.2.5 :", patch)
+    elif version.parse(patch) < version.parse("11.2.7.99999"):
         expansion_dict = {
-            "PTR": "ptr",
-            #"PTR2": "ptr-2",
+            #"PTR": "ptr",
+            "PTR2": "ptr-2",
         }
-        print("11.2.5 :", patch)
-    elif version.parse("11.2.5.99999") < version.parse(patch):
+        print("11.2.7 :", patch)
+    elif version.parse("11.2.7.99999") < version.parse(patch):
         expansion_dict = {
-            "PTR": "ptr",
-            #"PTR2": "ptr-2",
+            "BETA": "beta"
         }
         print("12.0.0 :", patch)
     return expansion_dict
