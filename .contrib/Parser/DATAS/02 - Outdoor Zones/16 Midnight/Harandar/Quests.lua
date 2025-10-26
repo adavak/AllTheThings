1b67d86a2d461e5c6bb00cfef55fb34e8afced5c
Alexandr Soft <37008350+AlexSoft1911@users.noreply.github.com>
2025-10-26 16:27:35 +0500

    Alpha: side quests Harandar (build 63854)

--- .contrib/Parser/DATAS/02 - Outdoor Zones/16 Midnight/Harandar/Quests.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/16 Midnight/Harandar/Quests.lua
@@ -491,6 +491,578 @@ root(ROOTS.Zones, m(MIDNIGHT, {
 				["coord"] = { 52.9, 51.7, HARANDAR },
 			}),
 			--Side quests
+			---1
+			q(90616, {	-- You Strong?
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 243226 },	-- Boletus
+				["coord"] = { 71.8, 64.0, HARANDAR },
+			}),
+			q(90617, {	-- A Few Fun Guys
+				["sourceQuests"] = { 90616 },	-- You Strong?
+				["provider"] = { "n", 243226 },	-- Boletus
+				["coord"] = { 71.8, 64.0, HARANDAR },
+			}),
+			q(90619, {	-- What Doesn't Kill Them
+				["sourceQuests"] = { 90617 },	-- A Few Fun Guys
+				["provider"] = { "n", 243226 },	-- Boletus
+				["coord"] = { 71.8, 64.0, HARANDAR },
+			}),
+			q(91450, {	-- We Ready Now
+				["sourceQuests"] = { 90619 },	-- What Doesn't Kill Them
+				["provider"] = { "n", 246208 },	-- Brakko
+				["coord"] = { 72.1, 62.9, HARANDAR },
+			}),
+			q(91270, {	-- The Most Important Thing
+				["sourceQuests"] = { 91450 },	-- We Ready Now
+				["provider"] = { "n", 251715 },	-- Tuktuk
+				["coord"] = { 71.8, 63.9, HARANDAR },
+				["groups"] = {
+					--TODO: bugged, Brakko uninteractable
+				},
+			}),
+			---2
+			q(92882, {	-- A Hunter's Plight
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 253390 },	-- Ketan
+				["coord"] = { 69.4, 52.8, HARANDAR },
+			}),
+			q(92883, {	-- A Hunter's Duty
+				["sourceQuests"] = { 92882 },	-- A Hunter's Plight
+				["provider"] = { "n", 253392 },	-- Akazi
+				["coord"] = { 70.5, 50.7, HARANDAR },
+				["groups"] = {
+					i(255259),	-- Chloroceros Bone (QI!)
+					i(255258),	-- Grovecrawler Fang (QI!)
+					i(255260),	-- Lasher Vine (QI!)
+					o(584525, {	-- 
+						i(255261),	-- Sporeglider's Tail Spore (QI!)
+					}),
+				},
+			}),
+			q(92884, {	-- A Hunter's Weapon
+				["sourceQuests"] = { 92883 },	-- A Hunter's Duty
+				["provider"] = { "n", 253433 },	-- Ketan
+				["coord"] = { 70.0, 52.9, HARANDAR },
+				["groups"] = {
+					o(602759, {	-- 
+						i(257717),	-- Ka'dani Spear (QI!)
+					}),
+				},
+			}),
+			q(92885, {	-- A Hunter's Prey
+				["sourceQuests"] = { 92884 },	-- A Hunter's Weapon
+				["provider"] = { "n", 253392 },	-- Akazi
+				["coord"] = { 70.5, 50.7, HARANDAR },
+				["groups"] = {
+					i(260472),
+					i(260473),
+					i(260474),
+					i(260476),
+					i(260477),	-- Ka'shuk Trainee's Hatchet
+					i(260478),
+					i(260479),
+					i(260481),	-- Ka'shuk Trainee's Spear
+					i(260482),
+					i(260483),
+				},
+			}),
+			---3
+			q(92694, {	-- Dusk Among Pigments
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 252871 },	-- Shay'neia
+				["coord"] = { 70.5, 51.2, HARANDAR },
+			}),
+			q(92695, {	-- The Stroke of Storms
+				["sourceQuests"] = { 92694 },	-- Dusk Among Pigments
+				["provider"] = { "n", 252872 },	-- Orn'shan
+				["coord"] = { 74.0, 53.1, HARANDAR },
+			}),
+			q(92696, {	-- Colors Reborn Anew
+				["sourceQuests"] = { 92695 },	-- The Stroke of Storms
+				["provider"] = { "n", 252872 },	-- Orn'shan
+				["coord"] = { 72.4, 55.7, HARANDAR },
+				["groups"] = {
+					i(258571),	-- Mushroom Pigment (QI!)
+				},
+			}),
+			q(92697, {	-- Hues of Tomorrow
+				["sourceQuests"] = { 92696 },	-- Colors Reborn Anew
+				["provider"] = { "n", 252872 },	-- Orn'shan
+				["coord"] = { 72.4, 55.7, HARANDAR },
+				["groups"] = {
+					i(260523),	-- 
+					i(260524),	-- Paintmaker's Stirrer
+					i(260526),	-- Paintmaker's Rapier
+					i(260527),	-- Paintmaker's Walking Stick
+					i(260528),	-- 
+					i(260529),	-- 
+					i(260530),	-- 
+				},
+			}),
+			---4
+			q(92864, {	-- Feeding the Buds
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 253312 },	-- Ney'tar
+				["coord"] = { 69.5, 50.6, HARANDAR },
+			}),
+			q(92865, {	-- Dusk Among Pigments
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 253312 },	-- Ney'tar
+				["coord"] = { 69.5, 50.6, HARANDAR },
+				["groups"] = {
+					i(255577),	-- Chloroceros Bud (QI!)
+				},
+			}),
+			q(92866, {	-- Re-Hydra-ted
+				["sourceQuests"] = {
+					92864,	-- Feeding the Buds
+					92865,	-- Dusk Among Pigments
+				},
+				["provider"] = { "n", 253312 },	-- Ney'tar
+				["coord"] = { 69.5, 50.6, HARANDAR },
+				["groups"] = {
+					i(260705),	-- Assistant Botanist Leafy (PET!)
+				},
+			}),
+			---5
+			q(90467, {	-- Tales of the Sky
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 242358 },	-- Kuri
+				["coord"] = { 67.8, 27.5, HARANDAR },
+				["groups"] = {
+					o(529370, {	-- 
+						i(240484),	-- Stray Skyshards (QI!)
+					}),
+				},
+			}),
+			q(90468, {	-- Ugh, Chores!
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 242358 },	-- Kuri
+				["coord"] = { 67.8, 27.5, HARANDAR },
+				["groups"] = {
+					i(240485),	-- Intact Saptor Frond (QI!)
+				},
+			}),
+			q(90469, {	-- Carry On, Wayward Kuri
+				["sourceQuests"] = {
+					90467,	-- Tales of the Sky
+					90468,	-- Ugh, Chores!
+				},
+				["provider"] = { "n", 242689 },	-- Kamari
+				["coord"] = { 69.4, 29.2, HARANDAR },
+			}),
+			q(90470, {	-- Skyglass Scavenging
+				["sourceQuests"] = { 90469 },	-- Carry On, Wayward Kuri
+				["provider"] = { "n", 242691 },	-- Kuri
+				["coord"] = { 69.7, 26.6, HARANDAR },
+			}),
+			q(90474, {	-- The Legend of Aln'sharan
+				["sourceQuests"] = { 90470 },	-- Skyglass Scavenging
+				["provider"] = { "n", 242691 },	-- Kuri
+				["coord"] = { 69.7, 26.6, HARANDAR },
+				["groups"] = {
+					i(255826),	-- Mysterious Skyshards
+				},
+			}),
+			---6
+			q(91346, {	-- Supplicants to The Goddess
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 246607 },	-- Cyenna
+				["coord"] = { 65.4, 28.1, HARANDAR },
+			}),
+			q(91359, {	-- Fungal Lashers B Gone
+				["sourceQuests"] = { 91346 },	-- Supplicants to The Goddess
+				["provider"] = { "n", 246607 },	-- Cyenna
+				["coord"] = { 65.4, 28.1, HARANDAR },
+				["groups"] = {
+					i(246886),	-- Harmonious Lash (QI!)
+				},
+			}),
+			q(91360, {	-- Weeding Out the Unwanted
+				["sourceQuests"] = { 91346 },	-- Supplicants to The Goddess
+				["provider"] = { "n", 246607 },	-- Cyenna
+				["coord"] = { 65.4, 28.1, HARANDAR },
+			}),
+			q(91360, {	-- Weeding Out the Unwanted
+				["sourceQuests"] = {
+					91359,	-- Fungal Lashers B Gone
+					91346,	-- Supplicants to The Goddess
+				},
+				["provider"] = { "n", 246607 },	-- Cyenna
+				["coord"] = { 65.4, 28.1, HARANDAR },
+				["groups"] = {
+					i(260432),	-- Riverscale's Choker
+					i(260431),	-- Riverscale's Collar
+				},
+			}),
+			---7
+			q(91063, {	-- The Blooming Lattice
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 245637 },	-- Su'meera
+				["coord"] = { 65.4, 22.6, HARANDAR },
+			}),
+			q(91065, {	-- Purloining Petals
+				["sourceQuests"] = { 91063 },	-- The Blooming Lattice
+				["provider"] = { "n", 245639 },	-- Su'meera
+				["coord"] = { 60.8, 29.9, HARANDAR },
+				["groups"] = {
+					o(550011, {
+						i(245886),	-- Paint-Speckled Gourd (QI!)
+					}),
+					o(550008, {
+						i(245883),	-- Splattered Scroll (QI!)
+					}),
+					o(548703, {
+						i(245570),	-- Pilfered Crafting Drill (QI!)
+					}),
+					o(548700, {
+						i(245569),	-- Well-worn Ladle (QI!)
+					}),
+				},
+			}),
+			q(91085, {	-- Petal Bristles
+				["sourceQuests"] = { 91063 },	-- The Blooming Lattice
+				["provider"] = { "n", 245639 },	-- Su'meera
+				["coord"] = { 60.8, 29.9, HARANDAR },
+				["groups"] = {
+					o(557986, {
+						i(245571),	-- Petalwing Plume (QI!)
+					}),
+				},
+			}),
+			q(91086, {	-- Nipping the Buds
+				["sourceQuests"] = { 91063 },	-- The Blooming Lattice
+				["provider"] = { "n", 245639 },	-- Su'meera
+				["coord"] = { 60.8, 29.9, HARANDAR },
+			}),
+			q(91088, {	-- Behind the Falls
+				["sourceQuests"] = {
+					91086,	-- Nipping the Buds
+					91085,	-- Petal Bristles
+					91065,	-- Purloining Petals
+				},
+				["provider"] = { "n", 245639 },	-- Su'meera
+				["coord"] = { 60.8, 29.9, HARANDAR },
+			}),
+			q(91136, {	-- Memories in Stone
+				["sourceQuests"] = { 91088 },	-- Behind the Falls
+				["provider"] = { "n", 245986 },	-- Su'meera
+				["coord"] = { 56.1, 24.8, HARANDAR },
+				["groups"] = {
+					i(260437),	-- Ja'kul's Woven Pants
+					i(260436),	-- Lost Mentor's Greaves
+					i(260435),	-- Painter's Stained Pantaloons
+					i(260434),	-- Vision-Walker's Leggings
+				},
+			}),
+			---8
+			q(91587, {	-- Carcass Cuisine
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 247936 },	-- Yu'relen
+				["coord"] = { 40.9, 23.2, HARANDAR },
+				["groups"] = {
+					i(246942),	-- Corpse Rind (QI!)
+				},
+			}),
+			q(91585, {	-- Fresh from the Garden
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 247936 },	-- Yu'relen
+				["coord"] = { 40.9, 23.2, HARANDAR },
+				["groups"] = {
+					i(246923),	-- Frillfish Fillet (QI!)
+					i(246924),	-- Root Drift Jelly (QI!)
+				},
+			}),
+			q(91586, {	-- Soil-based Alternatives
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 247936 },	-- Yu'relen
+				["coord"] = { 40.9, 23.2, HARANDAR },
+				["groups"] = {
+					o(557572, {	-- 
+						i(246926),	-- Nutrient Rich Soil (QI!)
+					}),
+				},
+			}),
+			q(91588, {	-- Harandar's Kitchen
+				["sourceQuests"] = {
+					91587,	-- Carcass Cuisine
+					91585,	-- Fresh from the Garden
+					91586,	-- Soil-based Alternatives
+				},
+				["provider"] = { "n", 247936 },	-- Yu'relen
+				["coord"] = { 40.9, 23.2, HARANDAR },
+			}),
+			q(91589, {	-- Root Dash Delivery
+				["sourceQuests"] = { 91588 },	-- Harandar's Kitchen
+				["provider"] = { "n", 247936 },	-- Yu'relen
+				["coord"] = { 40.9, 23.2, HARANDAR },
+				["groups"] = {
+					--PH reward (Pocket Lint)
+				},
+			}),
+			---9
+			q(90537, {	-- Late Bloomers
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 242650 },	-- Ney'leia
+				["coord"] = { 37.0, 26.0, HARANDAR },
+				["groups"] = {
+					o(544785, {	-- 
+						i(244337),	-- Empty Seed Sack (QI!)
+					}),
+				},
+			}),
+			q(90540, {	-- Rutaani Rescue
+				["sourceQuests"] = { 90537 },	-- Late Bloomers
+				["provider"] = { "n", 243053 },	-- Ney'leia
+				["coord"] = { 48.9, 29.7, HARANDAR },
+			}),
+			q(90569, {	-- Back in the Bag
+				["sourceQuests"] = { 90537 },	-- Late Bloomers
+				["provider"] = { "n", 243053 },	-- Ney'leia
+				["coord"] = { 48.9, 29.7, HARANDAR },
+				["groups"] = {
+					i(242226),	-- Stolen Seeds (QI!)
+				},
+			}),
+			q(90963, {	-- Caves of the Cleft
+				["sourceQuests"] = {
+					90569,	-- Back in the Bag
+					90540,	-- Rutaani Rescue
+				},
+				["provider"] = { "n", 243053 },	-- Ney'leia
+				["coord"] = { 48.9, 29.7, HARANDAR },
+			}),
+			q(90601, {	-- Gathering Glowshrooms
+				["sourceQuests"] = { 90963 },	-- Caves of the Cleft
+				["provider"] = { "n", 245166 },	-- Ney'leia
+				["coord"] = { 49.7, 23.3, HARANDAR },
+				["groups"] = {
+					o(531575, {	-- 
+						i(244429),	-- Glowshroom (QI!)
+					}),
+				},
+			}),
+			q(90602, {	-- Gomphusta
+				["sourceQuests"] = { 90963 },	-- Caves of the Cleft
+				["provider"] = { "n", 245166 },	-- Ney'leia
+				["coord"] = { 49.7, 23.3, HARANDAR },
+				["groups"] = {
+					i(246117),	-- Carved Key (QI!)
+					i(263473),	-- Recipe: Flora Frenzy (RECIPE!)
+					o(553808, {	-- 
+						i(246120),	-- Cultivation Notes (QI!)
+					}),
+				},
+			}),
+			---10
+			q(91872, {	-- The Former Rootwarden
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 237572 },	-- Hagar
+				["coord"] = { 34.9, 25.0, HARANDAR },
+			}),
+			q(91873, {	-- Buffer Zone
+				["sourceQuests"] = { 91872 },	-- The Former Rootwarden
+				["provider"] = { "n", 252495 },	-- Hagar
+				["coord"] = { 42.6, 34.1, HARANDAR },
+			}),
+			q(91874, {	-- Flare Up
+				["sourceQuests"] = { 91873 },	-- Buffer Zone
+				["provider"] = { "n", 252497 },	-- Hagar
+				["coord"] = { 42.3, 34.2, HARANDAR },
+			}),
+			q(91875, {	-- Natural Remedy
+				["sourceQuests"] = { 91874 },	-- Flare Up
+				["provider"] = { "n", 252496 },	-- Hagar
+				["coord"] = { 42.4, 34.4, HARANDAR },
+				["groups"] = {
+					i(252649),	-- Verdant Stalker Sludge (QI!)
+				},
+			}),
+			q(91876, {	-- Tending Hope
+				["sourceQuests"] = { 91875 },	-- Natural Remedy
+				["provider"] = { "n", 248886 },	-- Hagar
+				["coord"] = { 42.6, 33.6, HARANDAR },
+				["groups"] = {
+					i(260450),	-- Band of the Former Rootwarden
+					i(260451),	-- Ring of Hope Preserved
+				},
+			}),
+			---11
+			q(90533, {	-- Go Get Orweyna!
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 242593 },	-- Monte Gazlowe
+				["coord"] = { 47.1, 45.8, HARANDAR },
+				["groups"] = {
+					i(241125),	-- Handcrafted Plush (QI!)
+				},
+			}),
+			q(90534, {	-- The Home of the Haranir
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 242592 },	-- Orweyna
+				["coord"] = { 47.2, 45.8, HARANDAR },
+			}),
+			q(90535, {	-- Leave Your Mark
+				["sourceQuests"] = { 90534 },	-- The Home of the Haranir
+				["provider"] = { "n", 242882 },	-- Orweyna
+				["coord"] = { 51.3, 49.4, HARANDAR },
+				["groups"] = {
+					i(260427),	-- Nahuut's Second-Favorite Chew Toy
+				},
+			}),
+			---12
+			q(91550, {	-- A Game of Silence and Shadow
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 247640 },	-- Shao'mal
+				["coord"] = { 48.8, 44.3, HARANDAR },
+			}),
+			q(91551, {	-- De-nest-stration
+				["sourceQuests"] = { 91550 },	-- A Game of Silence and Shadow
+				["provider"] = { "n", 247658 },	-- Nayeli
+				["coord"] = { 53.9, 41.3, HARANDAR },
+			}),
+			q(91552, {	-- Feathered Fury
+				["sourceQuests"] = { 91550 },	-- A Game of Silence and Shadow
+				["provider"] = { "n", 247658 },	-- Nayeli
+				["coord"] = { 53.9, 41.3, HARANDAR },
+			}),
+			q(91553, {	-- Haranir Never Say Die!
+				["sourceQuests"] = {
+					91551,	-- De-nest-stration
+					91552,	-- Feathered Fury
+				},
+				["provider"] = { "n", 247736 },	-- Nayeli
+				["coord"] = { 57.3, 49.0, HARANDAR },
+				["groups"] = {
+					i(260439),	-- Silence and Shadow Champion's Badge
+				},
+			}),
+			---13
+			q(90824, {	-- My Brother's Alive!
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 244163 },	-- Chua
+				["coord"] = { 52.2, 55.1, HARANDAR },
+			}),
+			q(90826, {	-- The Healing Waters of Ahl'ua
+				["sourceQuests"] = { 90824 },	-- My Brother's Alive!
+				["provider"] = { "n", 244242 },	-- En'liahn
+				["coord"] = { 43.1, 61.4, HARANDAR },
+				["groups"] = {
+					i(246118),	-- En'liahn's Jug (QI!)
+					o(539208, {
+						i(243196),	-- Healing Waters of Ahl'ua (QI!)
+					}),
+				},
+			}),
+			q(90827, {	-- Only the Poisonous Parts
+				["sourceQuests"] = { 90824 },	-- My Brother's Alive!
+				["provider"] = { "n", 244242 },	-- En'liahn
+				["coord"] = { 43.1, 61.4, HARANDAR },
+				["groups"] = {
+					i(243598),	-- Phytogenic Poison Part (QI!)
+				},
+			}),
+			q(90829, {	-- Meeting My Mentor
+				["sourceQuests"] = {
+					90827,	-- Only the Poisonous Parts
+					90826,	-- The Healing Waters of Ahl'ua
+				},
+				["provider"] = { "n", 244242 },	-- En'liahn
+				["coord"] = { 43.1, 61.4, HARANDAR },
+				["groups"] = {
+					i(243598),	-- Phytogenic Poison Part (QI!)
+				},
+			}),
+			q(90830, {	-- The Path Will Reveal Itself
+				["sourceQuests"] = { 90829 },	-- Meeting My Mentor
+				["provider"] = { "n", 244394 },	-- En'liahn
+				["coord"] = { 63.9, 54.7, HARANDAR },
+			}),
+			q(90831, {	-- Doing Is Becoming
+				["sourceQuests"] = { 90829 },	-- Meeting My Mentor
+				["provider"] = { "n", 244419 },	-- Amarakk
+				["coord"] = { 63.9, 54.7, HARANDAR },
+			}),
+			q(90832, {	-- As Her Voice Goes Silent
+				["sourceQuests"] = {
+					90831,	-- Doing Is Becoming
+					90830,	-- The Path Will Reveal Itself
+				},
+				["provider"] = { "n", 244455 },	-- Amarakk
+				["coord"] = { 62.9, 62.4, HARANDAR },
+			}),
+			q(90833, {	-- The Final Rite
+				["sourceQuests"] = { 90832 },	-- As Her Voice Goes Silent
+				["provider"] = { "n", 244455 },	-- Amarakk
+				["coord"] = { 62.9, 62.4, HARANDAR },
+			}),
+			--[[q(, {	-- From This Point Forward (TODO: forgot to copy-paste, again)
+				["sourceQuests"] = { 90833 },	-- The Final Rite
+				["provider"] = { "n", 244473 },	-- En'liahn
+				["coord"] = { 62.9, 62.4, HARANDAR },
+				["groups"] = {
+					--PH
+				},
+			}),]]--
+			---14	(build 63854 - this quest chain lock out quest in other zone, could be fixed in future build or map bug)
+			q(91377, {	-- Spawn of the Dead
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 246777 },	-- Ghikal
+				["coord"] = { 43.9, 71.7, HARANDAR },
+			}),
+			q(91376, {	-- Little Monsters
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 246777 },	-- Ghikal
+				["coord"] = { 43.9, 71.7, HARANDAR },
+				["groups"] = {
+					i(247243),	-- Strange Mycelium (QI!)
+				},
+			}),
+			q(91379, {	-- Decayed Land
+				["sourceQuests"] = {
+					91376,	-- Little Monsters
+					91377,	-- Spawn of the Dead
+				},
+				["provider"] = { "n", 246777 },	-- Ghikal
+				["coord"] = { 44.1, 66.4, HARANDAR },
+			}),
+			q(91378, {	-- You Are Legend
+				["sourceQuests"] = {
+					91376,	-- Little Monsters
+					91377,	-- Spawn of the Dead
+				},
+				["provider"] = { "n", 246777 },	-- Ghikal
+				["coord"] = { 44.1, 66.4, HARANDAR },
+			}),
+			q(91381, {	-- Reticent Evil
+				["sourceQuests"] = {
+					91379,	-- Decayed Land
+					91378,	-- You Are Legend
+				},
+				["provider"] = { "n", 246777 },	-- Ghikal
+				["coord"] = { 44.1, 66.4, HARANDAR },
+				["groups"] = {
+					i(260440),	-- Blooming Spore Vestments
+					i(260447),	-- Fungus-Infested Brigandine
+					i(260444),	-- Ghikal's Spare Tunic
+					i(260446),	-- Spore-Covered Breastplate
+				},
+			}),
+			--Leading to villages, could be breadcrumb but they are not?
+			q(93745, {	-- Har'athir, Village of Memory
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 256245 },	-- Orna
+				["coord"] = { 54.2, 53.3, HARANDAR },
+			}),
+			q(93775, {	-- Har'mara, Village of Balance
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 256350 },	-- Ravi
+				["coord"] = { 50.7, 55.7, HARANDAR },
+			}),
+			q(93771, {	-- Har'kuai, Village of Rain
+				["sourceQuests"] = { 86864 },	-- Watch The Den
+				["provider"] = { "n", 256313 },	-- Kaleo
+				["coord"] = { 51.0, 50.9, HARANDAR },
+			}),
 		}),
 	}),
 }));
