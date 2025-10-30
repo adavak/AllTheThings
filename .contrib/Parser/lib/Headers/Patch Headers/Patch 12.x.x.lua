6d63396e0c36551c01795f866874a0432f419723
gbrage <gbrage@kth.se>
2025-10-30 21:11:51 +0100

    Patch 12.x.x header

---
+++ .contrib/Parser/lib/Headers/Patch Headers/Patch 12.x.x.lua
@@ -0,0 +1,9 @@
+P12xx = createHeader({
+	readable = "Patch 12.x.x",
+	icon = [[~_.asset("Expansion_MN")]],
+	text = {
+		en = "Patch 12.x.x",
+		es = "Parche 12.x.x",
+		mx = "Parche 12.x.x",
+	},
+});
