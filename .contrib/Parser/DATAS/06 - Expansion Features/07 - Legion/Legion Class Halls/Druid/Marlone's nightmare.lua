f5cdc1f0172a5139b76fac6f6b0a629202cfdf8d
Eremeir <20992519+Eremeir@users.noreply.github.com>
2025-11-03 15:35:54 -0600

    Malorne

--- .contrib/Parser/DATAS/06 - Expansion Features/07 - Legion/Legion Class Halls/Druid/Marlone's nightmare.lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/07 - Legion/Legion Class Halls/Druid/Marlone's nightmare.lua
@@ -5,7 +5,7 @@
 root(ROOTS.ExpansionFeatures, expansion(EXPANSION.LEGION, bubbleDown({ ["timeline"] = { ADDED_7_0_3 } },{
     n(CLASS_HALL, {
         cl(DRUID, bubbleDown({ ["classes"] = { DRUID } }, {
-            m(MARLONE_NIGHTMARE, {
+            m(MALORNE_NIGHTMARE, {
                 ["lore"] = "Malorne's Nightmare is an Emerald Nightmare recreation of a battle on the slopes of Mount Hyjal during the War of the Ancients in which Malorne and the other Wild Gods were slain. It is located in the Rift of Aln. The Archdruid of the Dreamgrove enters this Nightmare, along with several other druids, in order to save Malorne from the Nightmare's grasp.",
                 ["timeline"] = { ADDED_7_0_3 },
                 ["icon"] = 409547,
