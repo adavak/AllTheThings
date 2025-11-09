4aaaecbcb6a42aa53bea50b140d9876a91ade9b7
ShadowBrooks <63622031+ShadowBrooks@users.noreply.github.com>
2025-11-09 16:41:27 -0700

    Update Mardum The Shattered Abyss (Demon Hunter).lua

--- .contrib/Parser/DATAS/06 - Expansion Features/07 - Legion/Legion Class Halls/Mardum The Shattered Abyss (Demon Hunter).lua
+++ .contrib/Parser/DATAS/06 - Expansion Features/07 - Legion/Legion Class Halls/Mardum The Shattered Abyss (Demon Hunter).lua
@@ -346,7 +346,7 @@ root(ROOTS.ExpansionFeatures, expansion(EXPANSION.LEGION, bubbleDown({ ["timelin
 						["sourceQuests"] = {
 							47137,	-- Champions of Legionfall
 							-- #IF AFTER 7.2.0
-							43412	-- A Hero's Weapon
+							43412,	-- A Hero's Weapon
 							-- #ELSE
 							45998,	-- Investigate the Broken Shore
 							-- #ENDIF
