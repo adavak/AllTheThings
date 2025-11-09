50643f03fcdd54c535a7745277807791298129dd
Slackluster <ruben.sluimer@gmail.com>
2025-11-09 18:47:38 +0100

    Updated the description for Putting the Finite in Infinite

--- .contrib/Parser/DATAS/07 - Events/Remix Legion/1 - Achievements.lua
+++ .contrib/Parser/DATAS/07 - Events/Remix Legion/1 - Achievements.lua
@@ -35,7 +35,7 @@ root(ROOTS.WorldEvents, applyevent(EVENTS.REMIX_LEGION, n(REMIX_LEGION, {
 		ach(42807),	-- Cloudy With a Chance of Infernals
 		ach(42593),	-- Pillars of Creation
 		ach(61339, {	-- Putting the Finite in Infinite
-			["description"] = "Awarded to players who spend Infinite Power on 999 purchases of the Artifact Weapon [Limits Unbound] perk & complete a Keystone intime with a key level of 49 or greater.",
+			["description"] = ["description"] = "Earned upon reaching 999 Infinite Power or completing a +49 or higher Keystone in time.",
 		}),
 		ach(42313),	-- Remixing Time
 		ach(60959),	-- Tenured in the Timeways I
