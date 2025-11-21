aef1f2cef7a2fc075756e4904ab4e7bacc74940c
Crieve <dylanfortune@live.com>
2025-11-21 10:16:23 -0600

    Legion: Added Flight Masters to the Broken Shore.

--- .contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Broken Shore.lua
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/08 Broken Isles/Broken Shore.lua
@@ -234,16 +234,20 @@ root(ROOTS.Zones, {
 				}),
 				n(FLIGHT_PATHS, {
 					fp(1942, {	-- Aalgen Point, Broken Shore
+						["cr"] = 120118,	-- Heidirk the Scalekeeper <Flight Master>
 						["coord"] = { 70.6, 47.6, BROKEN_SHORE },
 					}),
 					fp(1862, {	-- Acherus: The Ebon Hold
+						["cr"] = 93465,	-- Grimwing <Flight Master>
 						["classes"] = { DEATHKNIGHT },
 						["coord"] = { 97.4, 56.0, BROKEN_SHORE },
 					}),
 					fp(1941, {	-- Deliverance Point, Broken Shore
+						["cr"] = 120272,	-- Illidari Calia <Flight Master>
 						["coord"] = { 45.2, 64.0, BROKEN_SHORE },
 					}),
 					fp(1856, {	-- Vengeance Point, Broken Shore
+						["cr"] = 111323,	-- Izal Whitemoon <Illidari Flight Master>
 						["coord"] = { 49.6, 21.0, BROKEN_SHORE },
 					}),
 				}),
