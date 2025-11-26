531a16d3e651456654127bab7ff156bbf3b51dab
Chun-Chi Hung <b10102229@gmail.com>
2025-11-26 23:29:33 +0800

    [Locale] Update and sort supported locales.

--- .contrib/Parser/DATAS/00 - DB/Parser Setup Data/SUPPORTED_LOCALES.lua
+++ .contrib/Parser/DATAS/00 - DB/Parser Setup Data/SUPPORTED_LOCALES.lua
@@ -1,5 +1,15 @@
 -- Represents the set of Locale identifiers which will be exported from parsed data, as available
 
 SUPPORTED_LOCALES = {
-	"en", "es", "de", "fr", "it", "pt", "ru", "ko", "cn"
+	"en",
+	"de",
+	"es",
+	"mx",
+	"fr",
+	"it",
+	"ko",
+	"pt",
+	"ru",
+	"cn",
+	"tw",
 }
