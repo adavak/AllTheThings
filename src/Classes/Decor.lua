5d26b89ebcdfe5ea8ab8be7369d6fb0e23e4a912
ImUnicke <ImUnicke+github@gmail.com>
2025-12-01 13:56:57 -0700

    [Logic] Retail: Added basic Decor collection functionality

--- src/Classes/Decor.lua
+++ src/Classes/Decor.lua
@@ -5,16 +5,23 @@ local _, app = ...
 local CACHE = "Decor"
 local CLASSNAME = "Decor"
 local KEY = "decorID"
-if app.GameBuildVersion < 110205 then	-- TODO: Change to 110207
-	app.CreateDecor = app.CreateUnimplementedClass(CLASSNAME, KEY)
+if not C_HousingCatalog then
+	-- If Decor isn't distinguishable yet, simply create an extended Item
+	app.CreateDecor = app.ExtendClass("Item", CLASSNAME, KEY, {});
 
 	return
 end
 
+local C_HousingCatalog_GetCatalogEntryInfoByItem
+	= C_HousingCatalog.GetCatalogEntryInfoByItem
+
+local pairs
+ 	= pairs
+
 -- Decor Lib [STUB -- WIP]
 do
 	app.CreateDecor = app.ExtendClass("Item", CLASSNAME, KEY, {
-		RefreshCollectionOnly = true,
+		CACHE = function() return CACHE end,
 		collectible = function(t) return app.Settings.Collectibles[CACHE]; end,
 		collected = function(t) return app.IsAccountCached(CACHE, t.decorID) and 1 end,
 	});
@@ -22,13 +29,23 @@ do
 	app.AddEventHandler("OnSavedVariablesAvailable", function(currentCharacter, accountWideData)
 		if not accountWideData[CACHE] then accountWideData[CACHE] = {} end
 	end)
-	-- app.AddEventHandler("OnRefreshCollections", function()
-	-- 	local saved, none = {}, {}
+	app.AddEventHandler("OnRefreshCollections", function()
+		local state, source
+		local saved, none = {}, {}
+		for id,sources in pairs(app.GetRawFieldContainer(KEY)) do
+			source = sources[1]
+			state = C_HousingCatalog_GetCatalogEntryInfoByItem(source.itemID, true)
+			-- quantity is how many owned
+			state = state and state.quantity > 0 or nil
+			if state ~= nil then
+				saved[id] = true
+			else
+				none[id] = true
+			end
+		end
 
-	-- 	-- TODO: refresh handling for Decor
-
-	-- 	-- Account Cache
-	-- 	app.SetBatchAccountCached(CACHE, saved, 1)
-	-- 	app.SetBatchAccountCached(CACHE, none)
-	-- end)
+		-- Account Cache
+		app.SetBatchAccountCached(CACHE, saved, 1)
+		app.SetBatchAccountCached(CACHE, none)
+	end)
 end
