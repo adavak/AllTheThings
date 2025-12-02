1ab9d629f758682f912eedc876f05aff58e7d3c7
Darkal <michalbert@seznam.cz>
2025-12-02 20:58:06 +0100

    Forgot to push Boralus Explorations yesterday.

---
+++ .contrib/Parser/DATAS/02 - Outdoor Zones/10 Kul Tiras/1 Boralus/Exploration.lua
@@ -0,0 +1,28 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+
+root(ROOTS.Zones, m(KUL_TIRAS, bubbleDown({ ["timeline"] = { ADDED_8_0_1 } }, {
+	m(BORALUS, {
+		n(EXPLORATION, {
+			visit_exploration(9407,{coord={55.8,30.4,BORALUS}}),	-- Ashvane Company Yards
+			visit_exploration(9980,{coord={52.3,34.4,BORALUS}}),	-- Ashvane Docks
+			exploration(8717),	-- Boralus Harbor
+			visit_exploration(9627,{coord={66.8,55.8,BORALUS}}),	-- Crosswind Commons
+			visit_exploration(9979,{coord={64.6,43.8,BORALUS}}),	-- Dampwick Ward
+			visit_exploration(9802,{coord={67.1,23.2,BORALUS}}),	-- Harbormaster's Office
+			visit_exploration(9588,{coord={48.6,36.9,BORALUS}}),	-- Hook Point
+			visit_exploration(9602,{coord={74.3,77.3,BORALUS}}),	-- Mariner's Row
+			visit_exploration(9765,{coord={37.0,58.1,BORALUS}}),	-- Proudmoore Academy
+			visit_exploration(9771,{coord={42.4,41.6,BORALUS}}),	-- Proudmoore Barracks
+			visit_exploration(8716,{coord={46.3,55.2,BORALUS}}),	-- Proudmoore Keep
+			visit_exploration(10017,{coord={70.6,16.9,BORALUS}}),	-- Sanctum of the Sages
+			visit_exploration(9801,{coord={73.5,13.6,BORALUS}}),	-- Snug Harbor Inn
+			visit_exploration(9554,{coord={57.9,83.1,BORALUS}}),	-- Stormsong Monastery
+			visit_exploration(10018,{coord={75.5,18.9,BORALUS}}),	-- Tradewinds Counting House
+			visit_exploration(8718,{coord={70.3,17.7,BORALUS}}),	-- Tradewinds Market
+			visit_exploration(9593,{coord={42.9,81.5,BORALUS}}),	-- Unity Square
+			visit_exploration(10034,{coord={54.2,61.5,BORALUS}}),	-- Upton Borough
+		}),
+	}),
+})));
