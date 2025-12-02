16a0ba5aba74ff8d7cfd871451c6da86bac6edb4
ImUnicke <ImUnicke+github@gmail.com>
2025-12-02 10:54:56 -0700

    [Logic] Retail: Attempted fix for Decor being cleared from ATT cache when failing to be refreshed due to API returning inaccurate data

--- src/Classes/Decor.lua
+++ src/Classes/Decor.lua
@@ -23,12 +23,14 @@ local C_HousingCatalog_GetCatalogEntryInfoByItem
 local pairs
  	= pairs
 
+local IsAccountCached = app.IsAccountCached
+
 -- Decor Lib [STUB -- WIP]
 do
 	app.CreateDecor = app.ExtendClass("Item", CLASSNAME, KEY, {
 		CACHE = function() return CACHE end,
 		collectible = function(t) return app.Settings.Collectibles[CACHE]; end,
-		collected = function(t) return app.IsAccountCached(CACHE, t.decorID) and 1 end,
+		collected = function(t) return IsAccountCached(CACHE, t.decorID) and 1 end,
 	});
 	app.AddSimpleCollectibleSwap(CLASSNAME, CACHE)
 	app.AddEventHandler("OnSavedVariablesAvailable", function(currentCharacter, accountWideData)
@@ -38,27 +40,26 @@ do
 		local state, source
 		local saved, none = {}, {}
 		for id,sources in pairs(app.GetRawFieldContainer(KEY)) do
-			source = sources[1]
-			state = C_HousingCatalog_GetCatalogEntryInfoByItem(source.itemID, true)
-			-- quantity is how many owned
-			-- hasPlaced is used when the item is owned, but placed in the house (or outside)
-			if state then
-				local sum = state.quantity + state.numPlaced
-				if sum > 0 and sum < 1000 then	-- Sometimes API returns 4294967295
-					-- state is valid, keep it
-				else
-					state = nil
+			if not IsAccountCached(CACHE, id) then
+				source = sources[1]
+				state = C_HousingCatalog_GetCatalogEntryInfoByItem(source.itemID, true)
+				-- quantity is how many owned
+				-- hasPlaced is used when the item is owned, but placed in the house (or outside)
+				if state then
+					local sum = state.quantity + state.numPlaced
+					if sum > 0 and sum < 1000 then	-- Sometimes API returns 4294967295
+						-- state is valid, keep it
+						saved[id] = true
+					else
+						none[id] = true
+					end
 				end
-			end
-			if state ~= nil then
-				saved[id] = true
-			else
-				none[id] = true
 			end
 		end
 
 		-- Account Cache
 		app.SetBatchAccountCached(CACHE, saved, 1)
-		app.SetBatchAccountCached(CACHE, none)
+		-- Decor is not currently reliably refreshed, so don't clear missing
+		-- app.SetBatchAccountCached(CACHE, none)
 	end)
 end
