678b5ade1906d3fd9a21e52360b96b6bf19422b2
ImUnicke <ImUnicke+github@gmail.com>
2025-12-03 00:56:26 -0700

    [Logic] Retail: Fixed a detection issue where certain Decor might not be marked collected when refreshing ATT due to the returned API data

--- src/Classes/Decor.lua
+++ src/Classes/Decor.lua
@@ -16,9 +16,10 @@ local C_HousingCatalog_GetCatalogEntryInfoByItem
 	= C_HousingCatalog.GetCatalogEntryInfoByItem
 
 -- TODO: test other APIs
--- /dump C_HousingCatalog.GetDecorMaxOwnedCount(2112) -> 0
--- /dump C_HousingCatalog.GetDecorTotalOwnedCount(2112) -> 0,0
--- /dump C_HousingCatalog.GetDecorTotalOwnedCount() -> 0,0
+-- this is non-parameterized, returns the max decor that can be owned
+-- /dump C_HousingCatalog.GetDecorMaxOwnedCount(2112) -> 5000
+-- this is non-parameterized, returns some quantity of decor owned and placed?
+-- /dump C_HousingCatalog.GetDecorTotalOwnedCount() -> 110,39
 
 local pairs
  	= pairs
@@ -49,8 +50,8 @@ do
 				-- quantity is how many owned
 				-- hasPlaced is used when the item is owned, but placed in the house (or outside)
 				if state then
-					local sum = state.quantity + state.numPlaced
-					if sum > 0 and sum < 1000 then	-- Sometimes API returns 4294967295
+					local sum = state.numStored + state.numPlaced
+					if sum > 0 and sum < 1000000 then	-- Sometimes API returns 4294967295
 						-- state is valid, keep it
 						saved[id] = true
 					else
