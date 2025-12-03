d86af8e7dcdc8fb4128f5e853e76d790f001dcf4
ImUnicke <ImUnicke+github@gmail.com>
2025-12-02 22:56:53 -0700

    [DB] Neighborhood Flightpaths are not faction-locked

--- .contrib/Parser/DATAS/12 - Housing/Flight Paths.lua
+++ .contrib/Parser/DATAS/12 - Housing/Flight Paths.lua
@@ -2,86 +2,70 @@
 --      H O U S I N G    M O D U L E       --
 ---------------------------------------------
 root(ROOTS.Housing, n(FLIGHT_PATHS, bubbleDownSelf({ ["timeline"] = { ADDED_11_2_7 } }, {
-	-- Alliance
+	-- Founder's Point
 	fp(3078, {	-- Brumewood Hollow, Founder's Point
 		["coord"] = { 56.3, 58.0, FOUNDERS_POINT },
 		["cr"] = 236118,	-- Lensholvada
-		["races"] = ALLIANCE_ONLY,
 	}),
 	fp(3072, {	-- Entrance Portal, Founder's Point
 		["coord"] = { 56.6, 27.7, FOUNDERS_POINT },
 		["cr"] = 236111,	-- Jeff
-		["races"] = ALLIANCE_ONLY,
 	}),
 	fp(3077, {	-- Gilded Oaks, Founder's Point
 		["coord"] = { 38.1, 62.8, FOUNDERS_POINT },
 		["cr"] = 236117,	-- Landan
-		["races"] = ALLIANCE_ONLY,
 	}),
 	fp(3074, {	-- Mear's Farm, Founder's Point
 		["coord"] = { 34.6, 34.2, FOUNDERS_POINT },
 		["cr"] = 236113,	-- Eibhlin
-		["races"] = ALLIANCE_ONLY,
 	}),
 	fp(3076, {	-- Small Aerie, Founder's Point
 		["coord"] = { 43.3, 44.9, FOUNDERS_POINT },
 		["cr"] = 236116,	-- Norbert
-		["races"] = ALLIANCE_ONLY,
 	}),
 	fp(3079, {	-- Stoneveil Ridge, Founder's Point
 		["coord"] = { 63.7, 41.4, FOUNDERS_POINT },
 		["cr"] = 236119,	-- Arneu
-		["races"] = ALLIANCE_ONLY,
 	}),
 	fp(3075, {	-- The Outer Banks, Founder's Point
 		["coord"] = { 32.4, 45.9, FOUNDERS_POINT },
 		["cr"] = 236115,	-- Niels
-		["races"] = ALLIANCE_ONLY,
 	}),
 	fp(3073, {	-- Town Center, Founder's Point
 		["coord"] = { 51.3, 39.6, FOUNDERS_POINT },
 		["cr"] = 236112,	-- Billson
-		["races"] = ALLIANCE_ONLY,
 	}),
-	-- Horde
+	-- Razorwind Shores
 	fp(2978, {	-- Cragthorn Highlands, Razorwind Shores
 		["coord"] = { 66.3, 56.5, RAZORWIND_SHORES },
 		["cr"] = 227952,	-- Jarg Riverbend
-		["races"] = HORDE_ONLY,
 	}),
 	fp(2972, {	-- Entrance Gate, Razorwind Shores
 		["coord"] = { 54.8, 51.2, RAZORWIND_SHORES },
 		["cr"] = 227801,	-- Drelda
-		["races"] = HORDE_ONLY,
 	}),
 	fp(2974, {	-- Runetotem's Bounty North, Razorwind Shores
 		["coord"] = { 42.7, 55.9, RAZORWIND_SHORES },
 		["cr"] = 227902,	-- Zurlik
-		["races"] = HORDE_ONLY,
 	}),
 	fp(2975, {	-- Runetotem's Bounty South, Razorwind Shores
 		["coord"] = { 47.8, 69.9, RAZORWIND_SHORES },
 		["cr"] = 227901,	-- Reginald Glarestone
-		["races"] = HORDE_ONLY,
 	}),
 	fp(2973, {	-- Saltfang Shoals East, Razorwind Shores
 		["coord"] = { 68.1, 75.6, RAZORWIND_SHORES },
 		["cr"] = 227894,	-- Trak Tuskbender
-		["races"] = HORDE_ONLY,
 	}),
 	fp(2980, {	-- The Bloom, Razorwind Shores
 		["coord"] = { 59.2, 71.8, RAZORWIND_SHORES },
 		["cr"] = 227896,	-- Wuls
-		["races"] = HORDE_ONLY,
 	}),
 	fp(2976, {	-- The Bluffs, Razorwind Shores
 		["coord"] = { 52.7, 81.7, RAZORWIND_SHORES },
 		["cr"] = 227900,	-- Luk'gra
-		["races"] = HORDE_ONLY,
 	}),
 	fp(2979, {	-- The Common, Razorwind Shores
 		["coord"] = { 53.6, 59.7, RAZORWIND_SHORES },
 		["cr"] = 227878,	-- Broktar
-		["races"] = HORDE_ONLY,
 	}),
 })));
