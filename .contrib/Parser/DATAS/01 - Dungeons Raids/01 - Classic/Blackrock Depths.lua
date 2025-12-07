46d54470537d722a5742808227dfb42b0d61abe8
ImUnicke <ImUnicke+github@gmail.com>
2025-12-06 22:57:05 -0700

    ...

--- .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/Blackrock Depths.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/01 - Classic/Blackrock Depths.lua
@@ -86,7 +86,7 @@ root(ROOTS.Instances, expansion(EXPANSION.CLASSIC, {
 					["timeline"] = { REMOVED_1_15_3 },
 					-- #endif
 					["cost"] = { { "i", 17203, 1 } },	-- Sulfuron Ingot
-					-- #IF NOT ANYCLASSIC
+					-- #IF ANYCLASSIC
 					["requireSkill"] = BLACKSMITHING,
 					-- #ENDIF
 					["lvl"] = lvlsquish(60, 60, 20),
