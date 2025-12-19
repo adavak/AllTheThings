00711aee0ed279515e6f7efcda6a3b1831cafd39
ImUnicke <ImUnicke+github@gmail.com>
2025-12-19 12:02:51 -0700

    [Logic] Retail: Revised logic for updating the Minimap Difficulty Icon using ATT Difficulties * Added a 2 second delay since (for me) it was registering as difficultyID 0 when testing originally

--- src/UI/Instance Difficulty Icon.lua
+++ src/UI/Instance Difficulty Icon.lua
@@ -2,26 +2,39 @@ local appName, app = ...
 
 if not app.IsRetail then return end
 
+-- Returns the Texture name to be swapped based on the DifficultyID. Defaults to "NormalTexture"
+local TextureNameByDifficultyID = setmetatable({
+	[2] = "HeroicTexture",
+	[8] = "MythicTexture",
+}, { __index = function(t,key) return "NormalTexture" end})
+-- HeroicTexture
+TextureNameByDifficultyID[5] = TextureNameByDifficultyID[2]
+TextureNameByDifficultyID[6] = TextureNameByDifficultyID[2]
+TextureNameByDifficultyID[11] = TextureNameByDifficultyID[2]
+TextureNameByDifficultyID[15] = TextureNameByDifficultyID[2]
+-- MythicTexture
+TextureNameByDifficultyID[9] = TextureNameByDifficultyID[8]
+TextureNameByDifficultyID[16] = TextureNameByDifficultyID[8]
+TextureNameByDifficultyID[23] = TextureNameByDifficultyID[8]
+
 local function changeMinimapSkull()
-	local _, instanceType, difficultyID, difficultyName, maxPlayers = GetInstanceInfo()
-	if difficultyID ~= 0 and (instanceType == "party" or instanceType == "raid") then
-		if MinimapCluster and MinimapCluster.InstanceDifficulty then
-			if difficultyID == 7 or difficultyID == 17 then
-				MinimapCluster.InstanceDifficulty.Default.NormalTexture:SetTexture(app.asset("Difficulty_LFR"))
-				MinimapCluster.InstanceDifficulty.Guild.Instance.NormalTexture:SetTexture(app.asset("Difficulty_LFR"))
-			elseif difficultyID == 1 or difficultyID == 3 or difficultyID == 4 or difficultyID == 14 then
-				MinimapCluster.InstanceDifficulty.Default.NormalTexture:SetTexture(app.asset("Difficulty_Normal"))
-				MinimapCluster.InstanceDifficulty.Guild.Instance.NormalTexture:SetTexture(app.asset("Difficulty_Normal"))
-			elseif difficultyID == 2 or difficultyID == 5 or difficultyID == 6 or difficultyID == 15 then
-				MinimapCluster.InstanceDifficulty.Default.HeroicTexture:SetTexture(app.asset("Difficulty_Heroic"))
-				MinimapCluster.InstanceDifficulty.Guild.Instance.HeroicTexture:SetTexture(app.asset("Difficulty_Heroic"))
-			elseif difficultyID == 8 or difficultyID == 16 or difficultyID == 23 then
-				MinimapCluster.InstanceDifficulty.Default.MythicTexture:SetTexture(app.asset("Difficulty_Mythic"))
-				MinimapCluster.InstanceDifficulty.Guild.Instance.MythicTexture:SetTexture(app.asset("Difficulty_Mythic"))
-			end
-		end
-	end
+	local _, instanceType, difficultyID = GetInstanceInfo()
+	if difficultyID == 0 or (instanceType ~= "party" and instanceType ~= "raid") then return end
+
+	local minimapClusterInstanceDifficulty = MinimapCluster and MinimapCluster.InstanceDifficulty
+	if not minimapClusterInstanceDifficulty then return end
+
+	local diff = app.CreateDifficulty(difficultyID)
+	local textureKey = TextureNameByDifficultyID[difficultyID]
+	minimapClusterInstanceDifficulty.Default[textureKey]:SetTexture(diff.icon)
+	minimapClusterInstanceDifficulty.Guild.Instance[textureKey]:SetTexture(diff.icon)
 end
 
-app.AddEventRegistration("UPDATE_INSTANCE_INFO", changeMinimapSkull)
-app.AddEventRegistration("RAID_INSTANCE_WELCOME", changeMinimapSkull)
+-- 2 second delayed callback to check the instance info and change the icon
+local function CallbackDifficultyIconUpdate()
+	app.CallbackHandlers.DelayedCallback(changeMinimapSkull, 2)
+end
+
+-- when this event fires, the difficultyID can still be detected as 0
+app.AddEventRegistration("UPDATE_INSTANCE_INFO", CallbackDifficultyIconUpdate)
+app.AddEventRegistration("RAID_INSTANCE_WELCOME", CallbackDifficultyIconUpdate)
