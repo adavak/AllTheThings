82d921553d027ac2b3851cc2eabf8ef804939637
Darkal <michalbert@seznam.cz>
2026-03-03 07:03:28 +0100

    Added new instanced battleground achievements.

---
+++ .contrib/Parser/DATAS/08 - PvP/Battlegrounds/Slayer's Rise.lua
@@ -0,0 +1,19 @@
+-----------------------------------------------
+--      P L A Y E R   V S   P L A Y E R      --
+-----------------------------------------------
+
+root(ROOTS.PVP, pvp(n(BATTLEGROUNDS, {
+	m(2397, {	-- Slayer's Rise
+		["icon"] = 7488401,
+		["timeline"] = { ADDED_12_0_1_LAUNCH },
+		["groups"] = {
+			n(ACHIEVEMENTS, {
+				ach(62517),	-- Rise of the Ultradon Slayer
+				ach(62516),	-- The Voided Gazelle
+				ach(62514),	-- Slayer's Rise Dominance
+				ach(62494),	-- Slayer's Rise Veteran
+				ach(62493),	-- Slayer's Rise Victory
+			}),
+		},
+	}),
+})));
