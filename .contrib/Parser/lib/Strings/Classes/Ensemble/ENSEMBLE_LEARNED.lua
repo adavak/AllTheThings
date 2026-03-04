d1e71e72e992f11b06453748a80d51de91260972
Darkal <michalbert@seznam.cz>
2026-03-05 00:46:00 +0100

    Ensembles are now fixed from Blizzard - removing part of the ensemble text.

--- .contrib/Parser/lib/Strings/Classes/Ensemble/ENSEMBLE_LEARNED.lua
+++ .contrib/Parser/lib/Strings/Classes/Ensemble/ENSEMBLE_LEARNED.lua
@@ -1,18 +1,18 @@
 createLocalizationString({
-	readable = "This Ensemble has already been used by one of your characters.\n\n|cFFA0A0A0Unfortunately, Blizzard’s system can sometimes fail to grant every item.|r",
+	readable = "This Ensemble has already been used by one of your characters.",
 	constant = "ENSEMBLE_LEARNED",
 	export = true,
 	text = {
-		en = "This Ensemble has already been used by one of your characters.\n\n|cFFA0A0A0Unfortunately, Blizzard’s system can sometimes fail to grant every item.|r",
+		en = "This Ensemble has already been used by one of your characters.",
 		-- TODO: de = "",
-		es = "Este conjunto ya ha sido usado por uno de tus personajes.\n\n|cFFA0A0A0Desafortunadamente, el sistema de Blizzard a veces puede fallar al otorgar todos los objetos.|r",
-		mx = "Este conjunto ya ha sido usado por uno de tus personajes.\n\n|cFFA0A0A0Desafortunadamente, el sistema de Blizzard a veces puede fallar al otorgar todos los objetos.|r",
+		es = "Este conjunto ya ha sido usado por uno de tus personajes.",
+		mx = "Este conjunto ya ha sido usado por uno de tus personajes.",
 		-- TODO: fr = "",
 		-- TODO: it = "",
 		-- TODO: pt = "",
 		-- TODO: ru = "",
 		-- TODO: ko = "",
-		cn = "该套装已被你的某个角色使用。\n\n|cFFA0A0A0很遗憾，暴雪的系统有时可能无法授予所有物品。|r",
+		cn = "该套装已被你的某个角色使用。",
 		-- TODO: tw = "",
 	},
 })
