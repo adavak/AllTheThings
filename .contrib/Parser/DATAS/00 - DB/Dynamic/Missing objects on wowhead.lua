62a8c32c3339abb84eb3640e801455a1ae489361
ShadowBrooks <63622031+ShadowBrooks@users.noreply.github.com>
2026-03-06 19:03:45 -0700

    More object's have names

---
+++ .contrib/Parser/DATAS/00 - DB/Dynamic/Missing objects on wowhead.lua
@@ -0,0 +1,63 @@
+﻿local ObjectDB = ObjectDB; for objectID,objectData in pairs({
+	[525932] = {
+		readable = "Half-Baked Techniques",
+		text = {
+			en = "Half-Baked Techniques",
+		},
+	},
+	[525952] = {
+		readable = "Enchanted Amani Mask",
+		text = {
+			en = "Enchanted Amani Mask",
+		},
+	},
+	[553966] = {
+		readable = "Pygmy Oil",
+		text = {
+			en = "Pygmy Oil",
+		},
+	},
+	[555823] = {
+		readable = "Herbalist's Handbook: Eversong Medicinals",
+		text = {
+			en = "Herbalist's Handbook: Eversong Medicinals",
+		},
+	},
+	[614767] = {
+		readable = "Giant Growth Potion",
+		text = {
+			en = "Giant Growth Potion",
+		},
+	},
+	[584503] = {
+		readable = "Grand Sanctified Spoils",
+		text = {
+			en = "Grand Sanctified Spoils",
+		},
+	},
+	[614789] = {
+		readable = "Inky Black Potion",
+		text = {
+			en = "Inky Black Potion",
+		},
+	},
+	[614803] = {
+		readable = "Savory Deviate Delight",
+		text = {
+			en = "Savory Deviate Delight",
+		},
+	},
+	[614821] = {
+		readable = "Savory Deviate Delight",
+		text = {
+			en = "Savory Deviate Delight",
+		},
+	},
+	[626980] = {
+		readable = "Belanise Cluster",
+		text = {
+			en = "Belanise Cluster",
+		},
+	},
+})
+do ObjectDB[objectID] = objectData; end
