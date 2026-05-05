e386b02c41d79daffff8c24fd01344ac386b5fc4
Darkal <michalbert@seznam.cz>
2026-05-05 21:28:29 +0200

    Fixed Skyreach loot? This dungeon is so messed up that this might also be wrong. We'll see more in future reports.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/06 - Warlords of Draenor/Skyreach.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/06 - Warlords of Draenor/Skyreach.lua
@@ -99,12 +99,92 @@ root(ROOTS.Instances, expansion(EXPANSION.WOD, bubbleDown({ ["timeline"] = { ADD
 			Difficulty(DIFFICULTY.DUNGEON.MULTI.HEROIC_PLUS).AddGroups({
 				BossOnly(RANJIT, {
 					ach(9033),	-- Ready for Raiding IV
+					i(258046, {	-- Chakram-Breaker Greatsword
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258574, {	-- Legwraps of Swirling Light
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258575, {	-- Rigid Scale Greatcloak
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258218, {	-- Skybreaker's Blade
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258412, {	-- Stormshaper's Crossbow
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
 				}),
 				BossOnly(ARAKNATH, {
+					i(258577, {	-- Boots of Burning Focus
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258436, {	-- Edge of the Burning Sun
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258579, {	-- Gutcrusher Greathelm
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258578, {	-- Lightbinder Shoulderguards
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258576, {	-- Sharpeye Chestguard
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(252418, {	-- Solar Core Igniter
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258047, {	-- Spire of the Furious Construct
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
 				}),
 				BossOnly(RUKHRAN, {
 					ach(9035, {	-- I Saw Solis
 						["crs"] = { 76227 },	-- Solar Flare
+					}),
+					i(258048, {	-- Beakbreaker Scimitar
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258438, {	-- Blazing Sunclaws
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258581, {	-- Bloodfeather Mantle
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258580, {	-- Bracers of Blazing Light
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258583, {	-- Incarnadine Gauntlets
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(252411, {	-- Radiant Sunstone
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258582, {	-- Rigid Scale Boots
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258472, {	-- Rukhran's Solar Reliquary
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
 					}),
 				}),
 				BossOnly(VIRYX, {
@@ -115,6 +195,38 @@ root(ROOTS.Instances, expansion(EXPANSION.WOD, bubbleDown({ ["timeline"] = { ADD
 					}),
 					ach(9036, {	-- Monomania
 						["crs"] = { 76292 },	-- Skyreach Shield Construct
+					}),
+					i(258050, {	-- Arcanic of the High Sage
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258586, {	-- Bloodfeather Chestguard
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258584, {	-- Lightbinder Treads
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258585, {	-- Sharpeye Gleam
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(252420, {	-- Solarflare Prism
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258587, {	-- Spaulders of Scorching Ray
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258484, {	-- Sunlance of Viryx
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
+					}),
+					i(258049, {	-- Viryx's Indomitable Bulwark
+						["timeline"] = { ADDED_12_0_1_SEASONSTART },
+						["ItemAppearanceModifierID"] = 0,
 					}),
 				}),
 			}),
@@ -143,91 +255,6 @@ root(ROOTS.Instances, expansion(EXPANSION.WOD, bubbleDown({ ["timeline"] = { ADD
 						["description"] = "Take this recipe to the \"Apexis Gemcutter\" in Tanaan Jungle to learn. If you have this recipe already you will need to revisit the vendor to cache the recipe.",
 					})
 				}),
-			}),
-
-			-- 12.0.0 revamp during Midnight Season 1
-			-- TODO: @ImUnicke (Runawaynow) I don't understand a single line in helpers.
-			-- Please figure out timelines/preprocessors for this file, thanks -Darkal
-			Difficulty(DIFFICULTY.DUNGEON.HEROIC).AddGroups({
-				BossOnly(RANJIT, {
-					i(258046),	-- Chakram-Breaker Greatsword
-					i(258574),	-- Legwraps of Swirling Light
-					i(258575),	-- Rigid Scale Greatcloak
-					i(258218),	-- Skybreaker's Blade
-					i(258412),	-- Stormshaper's Crossbow
-				}),
-				Boss(ARAKNATH, {
-					i(258577),	-- Boots of Burning Focus
-					i(258436),	-- Edge of the Burning Sun
-					i(258579),	-- Gutcrusher Greathelm
-					i(258578),	-- Lightbinder Shoulderguards
-					i(258576),	-- Sharpeye Chestguard
-					i(252418),	-- Solar Core Igniter
-					i(258047),	-- Spire of the Furious Construct
-				}),
-				Boss(RUKHRAN, {
-					i(258048),	-- Beakbreaker Scimitar
-					i(258438),	-- Blazing Sunclaws
-					i(258581),	-- Bloodfeather Mantle
-					i(258580),	-- Bracers of Blazing Light
-					i(258583),	-- Incarnadine Gauntlets
-					i(252411),	-- Radiant Sunstone
-					i(258582),	-- Rigid Scale Boots
-					i(258472),	-- Rukhran's Solar Reliquary
-				}),
-				Boss(VIRYX, {
-					i(258050),	-- Arcanic of the High Sage
-					i(258586),	-- Bloodfeather Chestguard
-					i(258584),	-- Lightbinder Treads
-					i(258585),	-- Sharpeye Gleam
-					i(252420),	-- Solarflare Prism
-					i(258587),	-- Spaulders of Scorching Ray
-					i(258484),	-- Sunlance of Viryx
-					i(258049),	-- Viryx's Indomitable Bulwark
-				}),
-			}),
-			Difficulty(DIFFICULTY.DUNGEON.MYTHIC, {["ItemAppearanceModifierID"] = 0}).AddGroups({
-				BossOnly(RANJIT, {
-					i(258046),	-- Chakram-Breaker Greatsword
-					i(258574),	-- Legwraps of Swirling Light
-					i(258575),	-- Rigid Scale Greatcloak
-					i(258218),	-- Skybreaker's Blade
-					i(258412),	-- Stormshaper's Crossbow
-				}),
-				Boss(ARAKNATH, {
-					i(258577),	-- Boots of Burning Focus
-					i(258436),	-- Edge of the Burning Sun
-					i(258579),	-- Gutcrusher Greathelm
-					i(258578),	-- Lightbinder Shoulderguards
-					i(258576),	-- Sharpeye Chestguard
-					i(252418),	-- Solar Core Igniter
-					i(258047),	-- Spire of the Furious Construct
-				}),
-				Boss(RUKHRAN, {
-					i(258048),	-- Beakbreaker Scimitar
-					i(258438),	-- Blazing Sunclaws
-					i(258581),	-- Bloodfeather Mantle
-					i(258580),	-- Bracers of Blazing Light
-					i(258583),	-- Incarnadine Gauntlets
-					i(252411),	-- Radiant Sunstone
-					i(258582),	-- Rigid Scale Boots
-					i(258472),	-- Rukhran's Solar Reliquary
-				}),
-				Boss(VIRYX, {
-					ach(10081),	-- Mythic: Skyreach
-					ig(127772, {	-- Gemcutter Module: Haste
-						["requireSkill"] = JEWELCRAFTING,
-						["description"] = "Take this recipe to the \"Apexis Gemcutter\" in Tanaan Jungle to learn. If you have this recipe already you will need to revisit the vendor to cache the recipe.",
-					}),
-					i(258050),	-- Arcanic of the High Sage
-					i(258586),	-- Bloodfeather Chestguard
-					i(258584),	-- Lightbinder Treads
-					i(258585),	-- Sharpeye Gleam
-					i(252420),	-- Solarflare Prism
-					i(258587),	-- Spaulders of Scorching Ray
-					i(258484),	-- Sunlance of Viryx
-					i(258049),	-- Viryx's Indomitable Bulwark
-				})
 			}),
 		},
 	}),
