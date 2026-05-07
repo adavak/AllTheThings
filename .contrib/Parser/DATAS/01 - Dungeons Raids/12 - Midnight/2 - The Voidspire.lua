160319c8dda1c94e8e85502de59c06b93dd70cbf
Darkal <michalbert@seznam.cz>
2026-05-07 18:03:55 +0200

    Fixed name of Blinky in Voidspire raid.

--- .contrib/Parser/DATAS/01 - Dungeons Raids/12 - Midnight/2 - The Voidspire.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/12 - Midnight/2 - The Voidspire.lua
@@ -331,7 +331,8 @@ root(ROOTS.Instances, expansion(EXPANSION.MID, bubbleDownSelf({ ["timeline"] = {
 					i(268460, {["timeline"] = { ADDED_12_0_5, REMOVED_12_1_0 }}),	-- Nebulous Voidcache: Vorasius
 				}),
 				n(240864, {	-- Blinky
-					i(268274),	-- Blinky's Collar
+					["name"] = "Blinky",	-- Name does not resolve in the raid due to secrets
+					["groups"] = { i(268274) },	-- Blinky's Collar
 				}),
 				BossOnly(SALHADAAR, {
 					i(264672),	-- Cosmic Ritual Stone (TOY!)
