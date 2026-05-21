b2c02e3eb6d1a0f40f7a85f37ca174204f95099b
Goldenshacal <goldenshacal@gmail.com>
2026-05-22 00:46:36 +0200

    split void assaults & ritual sites as blizzard seems to keep adding content to them and it might get complicated very quickly soon if it stayed as 1 file

---
+++ .contrib/Parser/lib/Headers/Expansion Features/12 - Midnight/Ritual Sites&Void Assaults.lua
@@ -0,0 +1,34 @@
+VOID_ASSAULTS = createHeader({
+	readable = "Void Assaults",
+	icon = [[~_.asset("Category_Void_Assaults")]],
+	text = {
+		en = "Void Assaults",
+		de = "Angriffe der Leere",
+		es = "Asaltos del Vacío",
+		mx = "Asaltos del Vacío",
+		fr = "Assauts du Vide",
+		it = "Assalti del Vuoto",
+		ko = "공허 공세",
+		pt = "Investidas do Caos",
+		ru = "Натиски Бездны",
+		cn = "虚空侵袭",
+		tw = "虛無襲擊",
+	},
+});
+RITUAL_SITES = createHeader({
+	readable = "Ritual Sites",
+	icon = [[~_.asset("Category_Ritual_Sites")]],
+	text = {
+		en = "Ritual Sites",
+		de = "Ritualstätten",
+		es = "Lugares de ritual",
+		mx = "Sitios de ritual",
+		fr = "Sites rituels",
+		it = "Siti Rituali",
+		ko = "의식 지점",
+		pt = "Sítios Ritualísticos",
+		ru = "Места проведения ритуалов",
+		cn = "仪式场地",
+		tw = "儀式地點第",
+	},
+});
