7c5bac6d4da0d990ce0d127ad89acc370ab8ef87
Eremeir <20992519+Eremeir@users.noreply.github.com>
2026-06-20 13:44:24 -0500

    Update Hellfire Ramparts with InstanceHelper

--- .contrib/Parser/DATAS/01 - Dungeons Raids/02 - Burning Crusade/Hellfire Ramparts.lua
+++ .contrib/Parser/DATAS/01 - Dungeons Raids/02 - Burning Crusade/Hellfire Ramparts.lua
@@ -2,6 +2,23 @@
 --   D U N G E O N S  &  R A I D S  M O D U L E    --
 -----------------------------------------------------
 
+------ Encounter Constants ------
+local GARGOLMAR = 527;
+local OMOR = 528;
+local VAZRUDEN = 529;
+
+------ EncounterToCRS ------
+local EncounterToCRS = {
+	[GARGOLMAR] = { 17306 },	-- Watchkeeper Gargolmar
+	[OMOR] = { 17308 },	-- Omor the Unscarred
+	[VAZRUDEN] = { 17307 },	-- Vazruden the Herald
+};
+
+------ Boss Functions ------
+local InstanceHelper = CreateInstanceHelper(EncounterToCRS)
+local BossOnly, Difficulty =
+InstanceHelper.BossOnly, InstanceHelper.Difficulty
+
 root(ROOTS.Instances, expansion(EXPANSION.TBC, applyclassicphase(TBC_PHASE_ONE, bubbleDownSelf({ ["timeline"] = { ADDED_2_0_1 } }, {
 	inst(248, {	-- Hellfire Ramparts
 		["lore"] = "Hellfire Ramparts make up the first wing of Hellfire Citadel. The citadel itself sits in the center of the zone, in the middle of a broken-up wall that divided the zone before the conflict began. The Ramparts takes place atop this wall along the sides of the citadel.",
@@ -122,122 +139,171 @@ root(ROOTS.Instances, expansion(EXPANSION.TBC, applyclassicphase(TBC_PHASE_ONE, 
 					},
 				},
 			}),
-			d(DIFFICULTY.DUNGEON.NORMAL, {
-				e(527, {	-- Watchkeeper Gargolmar
-					["creatureID"] = 17306,
-					["groups"] = {
-						i(24020),	-- Shadowrend Longblade
-						-- #if AFTER 7.3.5
-						i(27449),	-- Blood Knight Defender
-						-- #endif
-						i(24024),	-- Pauldrons of Arcane Rage
-						-- #if AFTER 7.3.5
-						i(27448),	-- Cloak of the Everliving
-						-- #endif
-						i(24021),	-- Light-Touched Breastplate
-						i(24023),	-- Bracers of Finesse
-						-- #if AFTER 7.3.5
-						i(27447),	-- Bracers of Just Rewards
-						-- #endif
-						i(24022),	-- Scale Leggings of the Skirmisher
-						-- #if AFTER 7.3.5
-						i(27451),	-- Boots of the Darkwalker
-						i(27450),	-- Wild Stalker Boots
-						-- #endif
-						i(23881),	-- Gargolmar's Hand
-					},
+			-- #if AFTER 7.3.5
+			Difficulty(DIFFICULTY.DUNGEON.MULTI.NORMAL_HEROIC).AddGroups({
+				BossOnly(GARGOLMAR, {
+					i(23881),	-- Gargolmar's Hand (QI!)
+					--
+					i(27449),	-- Blood Knight Defender
+					i(27451),	-- Boots of the Darkwalker
+					i(24023),	-- Bracers of Finesse
+					i(27447),	-- Bracers of Just Rewards
+					i(27448),	-- Cloak of the Everliving
+					i(24021),	-- Light-Touched Breastplate
+					i(24024),	-- Pauldrons of Arcane Rage
+					i(24022),	-- Scale Leggings of the Skirmisher
+					i(24020),	-- Shadowrend Longblade
+					i(27450),	-- Wild Stalker Boots
 				}),
-				e(528, {	-- Omor the Unscarred
-					["creatureID"] = 17308,
-					["groups"] = {
-						ach(647),	-- Hellfire Ramparts
-						i(24069),	-- Crystalfire Staff
-						i(24094),	-- Heart Fire Warhammer
-						-- #if AFTER 7.3.5
-						i(27463),	-- Terror Flame Dagger
-						i(27476),	-- Truncheon of Five Hells
-						i(27477),	-- Faol's Signet of Cleansing
-						i(27466),	-- Headdress of Alacrity
-						-- #endif
-						i(24073),	-- Garrote-String Necklace
-						i(24096),	-- Heartblood Prayer Beads
-						-- #if AFTER 7.3.5
-						i(27464),	-- Omor's Unyielding Will
-						i(27539),	-- Justice Bearer's Pauldrons
-						i(27906),	-- Crimsonforge Breastplate
-						i(27462),	-- Crimson Bracers of Gloom
-						-- #endif
-						i(24090),	-- Bloodstained Ravager Gauntlets
-						-- #if AFTER 7.3.5
-						i(27465),	-- Mana-Etched Gloves
-						i(27478),	-- Girdle of the Blasted Reaches
-						-- #endif
-						i(24091),	-- Tenacious Defender
-						-- #if AFTER 7.3.5
-						i(27467),	-- Silent-Strider Kneeboots
-						i(27895),	-- Band of Many Prisms
-						-- #endif
-						i(23886),	-- Omor's Hoof
-					},
+				BossOnly(OMOR, {
+					i(23886),	-- Omor's Hoof (QI!)
+					--
+					ach(647),	-- Hellfire Ramparts
+					i(27895),	-- Band of Many Prisms
+					i(24090),	-- Bloodstained Ravager Gauntlets
+					i(27462),	-- Crimson Bracers of Gloom
+					i(27906),	-- Crimsonforge Breastplate
+					i(24069),	-- Crystalfire Staff
+					i(27477),	-- Faol's Signet of Cleansing
+					i(24073),	-- Garrote-String Necklace
+					i(27478),	-- Girdle of the Blasted Reaches
+					i(27466),	-- Headdress of Alacrity
+					i(24094),	-- Heart Fire Warhammer
+					i(24096),	-- Heartblood Prayer Beads
+					i(27539),	-- Justice Bearer's Pauldrons
+					i(27465),	-- Mana-Etched Gloves
+					i(23886),	-- Omor's Hoof
+					i(27464),	-- Omor's Unyielding Will
+					i(27467),	-- Silent-Strider Kneeboots
+					i(24091),	-- Tenacious Defender
+					i(27463),	-- Terror Flame Dagger
+					i(27476),	-- Truncheon of Five Hells
 				}),
-				e(529, {	-- Vazruden the Herald
-					["creatureID"] = 17307,
-					["groups"] = {
-						i(23890, {	-- Ominous Letter [Alliance]
-							["races"] = ALLIANCE_ONLY,
-						}),
-						i(23892, {	-- Ominous Letter [Horde]
-							["races"] = HORDE_ONLY,
-						}),
-					},
+				BossOnly(VAZRUDEN, {
+					a(i(23890)),	-- Ominous Letter [Alliance]
+					h(i(23892)),	-- Ominous Letter [Horde]
 				}),
 				n(17536, {	-- Nazan
-					i(23901),	-- Nazan's Head
+					i(23901),	-- Nazan's Head (QI!)
 				}),
 				o(185168, {	-- Reinforced Fel Iron Chest
 					["description"] = "Available after defeating both Vazruden the Herald and Nazan.",
 					["crs"] = { 17307, 17536 },	-- Vazruden and Nazan
 					["groups"] = {
-						i(24044),	-- Hellreaver
-						i(24155),	-- Ursol's Claw
-						-- #if AFTER 7.3.5
-						i(29346),	-- Feltooth Eviscerator
-						i(27455),	-- Irondrake Faceguard
-						i(27454),	-- Volcanic Pauldrons
-						-- #endif
-						i(24150),	-- Mok'Nathal Wildercloak
-						-- #if AFTER 7.3.5
+						i(27453),	-- Averinn's Ring of Slaying
+						i(24045),	-- Band of Renewal
 						i(27461),	-- Chestguard of the Prowler
-						i(27456),	-- Raiments of Nature's Breath
-						i(27452),	-- Light Scribe Bands
-						i(27459),	-- Vambraces of Daring
+						i(29346),	-- Feltooth Eviscerator
+						i(24044),	-- Hellreaver
+						i(27455),	-- Irondrake Faceguard
+						i(24064),	-- Ironsole Clompers
+						i(24046),	-- Kilt of Rolling Thunders
 						i(27457),	-- Life Bearer's Gauntlets
-						i(32077),	-- Wrath-Infused Gauntlets
+						i(24083),	-- Lifegiver Britches
+						i(27452),	-- Light Scribe Bands
 						i(29238),	-- Lion's Heart Girdle
-						-- #endif
+						i(24151),	-- Mok'Nathal Clan Ring
+						i(24150),	-- Mok'Nathal Wildercloak
+						i(27458),	-- Oceansong Kilt
+						i(27456),	-- Raiments of Nature's Breath
+						i(27460),	-- Reavers' Ring
 						i(24063),	-- Shifting Sash of Midnight
-						-- #if AFTER 7.3.5
 						i(29264),	-- Tree-Mender's Belt
-						-- #endif
+						i(24155),	-- Ursol's Claw
+						i(27459),	-- Vambraces of Daring
+						i(27454),	-- Volcanic Pauldrons
+						i(24154),	-- Witching Band
+						i(32077),	-- Wrath-Infused Gauntlets
+					},
+				}),
+			}),
+			Difficulty(DIFFICULTY.DUNGEON.NORMAL).AddGroups({
+			}),
+			Difficulty(DIFFICULTY.DUNGEON.HEROIC, { ["lvl"] = lvlsquish(70, 70, 30) }).AddGroups({
+				n(COMMON_BOSS_DROPS, {
+					["crs"] = {
+						17306,	-- Watchkeeper Gargolmar
+						17308,	-- Omor the Unscarred
+						17307,	-- Vazruden the Herald
+					},
+					["groups"] = {
+						i(30593),	-- Potent Fire Opal
+						i(30594),	-- Regal Chrysoprase
+						i(30592),	-- Steady Chrysoprase
+					},
+				}),
+				ach(667, {	-- Heroic: Hellfire Ramparts
+					crit(3705, {	-- Omor the Unscarred
+						["_encounter"] = { OMOR, DIFFICULTY.DUNGEON.HEROIC },
+					}),
+					crit(3816, {	-- Vazruden
+						["_encounter"] = { VAZRUDEN, DIFFICULTY.DUNGEON.HEROIC },
+					}),
+				}),
+				ach(5067, { ["timeline"] = { ADDED_4_0_3 } }),	-- Heroic: Hellfire Ramparts Guild Run
+				BossOnly(OMOR, {
+					applyclassicphase(TBC_PHASE_ONE, i(23572)),	-- Primal Nether
+				}),
+				BossOnly(VAZRUDEN),
+				n(17536, {	-- Nazan
+					crit(3706, {	-- Nazan
+						["achievementID"] = 667,
+						["timeline"] = { ADDED_3_0_2 },
+					}),
+					i(33833),	-- Nazan's Riding Crop (QI!)
+				}),
+			}),
+			-- #else
+			Difficulty(DIFFICULTY.DUNGEON.MULTI.NORMAL_HEROIC).AddGroups({
+				BossOnly(GARGOLMAR, {
+					i(23881),	-- Gargolmar's Hand (QI!)
+				}),
+				BossOnly(OMOR, {
+					i(23886),	-- Omor's Hoof (QI!)
+				}),
+				n(17536, {	-- Nazan
+					i(23901),	-- Nazan's Head (QI!)
+				}),
+			}),
+			Difficulty(DIFFICULTY.DUNGEON.NORMAL).AddGroups({
+				BossOnly(GARGOLMAR, {
+					i(24023),	-- Bracers of Finesse
+					i(24021),	-- Light-Touched Breastplate
+					i(24024),	-- Pauldrons of Arcane Rage
+					i(24022),	-- Scale Leggings of the Skirmisher
+					i(24020),	-- Shadowrend Longblade
+				}),
+				BossOnly(OMOR, {
+					ach(647),	-- Hellfire Ramparts
+					i(24090),	-- Bloodstained Ravager Gauntlets
+					i(24069),	-- Crystalfire Staff
+					i(24073),	-- Garrote-String Necklace
+					i(24094),	-- Heart Fire Warhammer
+					i(24096),	-- Heartblood Prayer Beads
+					i(24091),	-- Tenacious Defender
+				}),
+				BossOnly(VAZRUDEN, {
+					a(i(23890)),	-- Ominous Letter [Alliance]
+					h(i(23892)),	-- Ominous Letter [Horde]
+				}),
+				o(185168, {	-- Reinforced Fel Iron Chest
+					["description"] = "Available after defeating both Vazruden the Herald and Nazan.",
+					["crs"] = { 17307, 17536 },	-- Vazruden and Nazan
+					["groups"] = {
+						i(24045),	-- Band of Renewal
+						i(24044),	-- Hellreaver
+						i(24064),	-- Ironsole Clompers
 						i(24046),	-- Kilt of Rolling Thunders
 						i(24083),	-- Lifegiver Britches
-						-- #if AFTER 7.3.5
-						i(27458),	-- Oceansong Kilt
-						-- #endif
-						i(24064),	-- Ironsole Clompers
-						-- #if AFTER 7.3.5
-						i(27453),	-- Averinn's Ring of Slaying
-						-- #endif
-						i(24045),	-- Band of Renewal
 						i(24151),	-- Mok'Nathal Clan Ring
-						-- #if AFTER 7.3.5
-						i(27460),	-- Reavers' Ring
-						-- #endif
+						i(24150),	-- Mok'Nathal Wildercloak
+						i(24063),	-- Shifting Sash of Midnight
+						i(24155),	-- Ursol's Claw
 						i(24154),	-- Witching Band
 					},
 				}),
 			}),
-			d(DIFFICULTY.DUNGEON.HEROIC, {
+			Difficulty(DIFFICULTY.DUNGEON.HEROIC, {
 				-- #if BEFORE 4.2.0
 				["description"] = "You need to have a key to the instance in order to access this mode.",
 				["cost"] = {
@@ -250,155 +316,83 @@ root(ROOTS.Instances, expansion(EXPANSION.TBC, applyclassicphase(TBC_PHASE_ONE, 
 				},
 				-- #endif
 				["lvl"] = lvlsquish(70, 70, 30),
-				["groups"] = {
-					ach(667, {	-- Heroic: Hellfire Ramparts
-						crit(3705, {	-- Omor the Unscarred
-							["_encounter"] = { 528, DIFFICULTY.DUNGEON.HEROIC },
-						}),
-						crit(3816, {	-- Vazruden
-							["_encounter"] = { 529, DIFFICULTY.DUNGEON.HEROIC },
-						}),
+			}).AddGroups({
+				n(COMMON_BOSS_DROPS, {
+					["crs"] = {
+						17306,	-- Watchkeeper Gargolmar
+						17308,	-- Omor the Unscarred
+						17307,	-- Vazruden the Herald
+					},
+					["groups"] = {
+						-- #if BEFORE CATA
+						BADGE_OF_JUSTICE(1),
+						-- #endif
+						i(30593),	-- Potent Fire Opal
+						i(30594),	-- Regal Chrysoprase
+						i(30592),	-- Steady Chrysoprase
+					},
+				}),
+				ach(667, {	-- Heroic: Hellfire Ramparts
+					crit(3705, {	-- Omor the Unscarred
+						["_encounter"] = { OMOR, DIFFICULTY.DUNGEON.HEROIC },
 					}),
-					ach(5067, {	-- Heroic: Hellfire Ramparts Guild Run
-						["timeline"] = { ADDED_4_0_3 },
+					crit(3816, {	-- Vazruden
+						["_encounter"] = { VAZRUDEN, DIFFICULTY.DUNGEON.HEROIC },
 					}),
-					n(COMMON_BOSS_DROPS, {
-						["crs"] = {
-							17306,	-- Watchkeeper Gargolmar
-							17308,	-- Omor the Unscarred
-							17307,	-- Vazruden the Herald
-						},
-						["groups"] = {
-							-- #if BEFORE CATA
-							BADGE_OF_JUSTICE(1),
-							-- #endif
-							i(30593),	-- Potent Fire Opal
-							i(30594),	-- Regal Chrysoprase
-							i(30592),	-- Steady Chrysoprase
-						},
+				}),
+				ach(5067, { ["timeline"] = { ADDED_4_0_3 } }),	-- Heroic: Hellfire Ramparts Guild Run
+				BossOnly(GARGOLMAR, {
+					i(27449),	-- Blood Knight Defender
+					i(27451),	-- Boots of the Darkwalker
+					i(27447),	-- Bracers of Just Rewards
+					i(27448),	-- Cloak of the Everliving
+					i(27450),	-- Wild Stalker Boots
+				}),
+				BossOnly(OMOR, {
+					i(27895),	-- Band of Many Prisms
+					i(27462),	-- Crimson Bracers of Gloom
+					i(27906),	-- Crimsonforge Breastplate
+					i(27477),	-- Faol's Signet of Cleansing
+					i(27478),	-- Girdle of the Blasted Reaches
+					i(27466),	-- Headdress of Alacrity
+					i(27539),	-- Justice Bearer's Pauldrons
+					i(27465),	-- Mana-Etched Gloves
+					i(27464),	-- Omor's Unyielding Will
+					applyclassicphase(TBC_PHASE_ONE, i(23572)),	-- Primal Nether
+					i(27467),	-- Silent-Strider Kneeboots
+					i(27463),	-- Terror Flame Dagger
+					i(27476),	-- Truncheon of Five Hells
+				}),
+				BossOnly(VAZRUDEN),
+				n(17536, {	-- Nazan
+					crit(3706, {	-- Nazan
+						["achievementID"] = 667,
+						["timeline"] = { ADDED_3_0_2 },
 					}),
-					e(527, {	-- Watchkeeper Gargolmar
-						["creatureID"] = 17306,
-						["groups"] = {
-							-- #if AFTER 7.3.5
-							i(24020),	-- Shadowrend Longblade
-							-- #endif
-							i(27449),	-- Blood Knight Defender
-							-- #if AFTER 7.3.5
-							i(24024),	-- Pauldrons of Arcane Rage
-							-- #endif
-							i(27448),	-- Cloak of the Everliving
-							-- #if AFTER 7.3.5
-							i(24021),	-- Light-Touched Breastplate
-							i(24023),	-- Bracers of Finesse
-							-- #endif
-							i(27447),	-- Bracers of Just Rewards
-							-- #if AFTER 7.3.5
-							i(24022),	-- Scale Leggings of the Skirmisher
-							-- #endif
-							i(27451),	-- Boots of the Darkwalker
-							i(27450),	-- Wild Stalker Boots
-							i(23881),	-- Gargolmar's Hand
-						},
-					}),
-					e(528, {	-- Omor the Unscarred
-						["creatureID"] = 17308,
-						["groups"] = {
-							-- #if AFTER 7.3.5
-							i(24069),	-- Crystalfire Staff
-							i(24094),	-- Heart Fire Warhammer
-							-- #endif
-							i(27463),	-- Terror Flame Dagger
-							i(27476),	-- Truncheon of Five Hells
-							i(27477),	-- Faol's Signet of Cleansing
-							i(27466),	-- Headdress of Alacrity
-							-- #if AFTER 7.3.5
-							i(24073),	-- Garrote-String Necklace
-							i(24096),	-- Heartblood Prayer Beads
-							-- #endif
-							i(27464),	-- Omor's Unyielding Will
-							i(27539),	-- Justice Bearer's Pauldrons
-							i(27906),	-- Crimsonforge Breastplate
-							i(27462),	-- Crimson Bracers of Gloom
-							-- #if AFTER 7.3.5
-							i(24090),	-- Bloodstained Ravager Gauntlets
-							-- #endif
-							i(27465),	-- Mana-Etched Gloves
-							i(27478),	-- Girdle of the Blasted Reaches
-							-- #if AFTER 7.3.5
-							i(24091),	-- Tenacious Defender
-							-- #endif
-							i(27467),	-- Silent-Strider Kneeboots
-							i(27895),	-- Band of Many Prisms
-							applyclassicphase(TBC_PHASE_ONE, i(23572)),	-- Primal Nether
-							i(23886),	-- Omor's Hoof
-						},
-					}),
-					e(529, {	-- Vazruden the Herald
-						["creatureID"] = 17307,
-						["groups"] = {
-							i(23890, {	-- Ominous Letter [Alliance]
-								["races"] = ALLIANCE_ONLY,
-							}),
-							i(23892, {	-- Ominous Letter [Horde]
-								["races"] = HORDE_ONLY,
-							}),
-						},
-					}),
-					n(17536, {	-- Nazan
-						crit(3706, {	-- Nazan
-							["achievementID"] = 667,
-							["timeline"] = { ADDED_3_0_2 },
-						}),
-						i(23901),	-- Nazan's Head
-						i(33833),	-- Nazan's Riding Crop
-					}),
-					o(185168, {	-- Reinforced Fel Iron Chest
-						["description"] = "Available after defeating both Vazruden the Herald and Nazan.",
-						["crs"] = { 17307, 17536 },	-- Vazruden and Nazan
-						["groups"] = {
-							-- #if AFTER 7.3.5
-							i(24044),	-- Hellreaver
-							i(24155),	-- Ursol's Claw
-							-- #endif
-							i(29346),	-- Feltooth Eviscerator
-							i(27455),	-- Irondrake Faceguard
-							i(27454),	-- Volcanic Pauldrons
-							-- #if AFTER 7.3.5
-							i(24150),	-- Mok'Nathal Wildercloak
-							-- #endif
-							i(27461),	-- Chestguard of the Prowler
-							i(27456),	-- Raiments of Nature's Breath
-							i(27452),	-- Light Scribe Bands
-							i(27459),	-- Vambraces of Daring
-							i(27457),	-- Life Bearer's Gauntlets
-							i(32077),	-- Wrath-Infused Gauntlets
-							i(29238),	-- Lion's Heart Girdle
-							-- #if AFTER 7.3.5
-							i(24063),	-- Shifting Sash of Midnight
-							-- #endif
-							i(29264),	-- Tree-Mender's Belt
-							-- #if AFTER 7.3.5
-							i(24046),	-- Kilt of Rolling Thunders
-							i(24083),	-- Lifegiver Britches
-							-- #endif
-							i(27458),	-- Oceansong Kilt
-							-- #if AFTER 7.3.5
-							i(24064),	-- Ironsole Clompers
-							-- #endif
-							i(27453),	-- Averinn's Ring of Slaying
-							-- #if AFTER 7.3.5
-							i(24045),	-- Band of Renewal
-							i(24151),	-- Mok'Nathal Clan Ring
-							-- #endif
-							i(27460),	-- Reavers' Ring
-							-- #if AFTER 7.3.5
-							i(24154),	-- Witching Band
-							-- #endif
-						},
-					}),
-				},
+					i(33833),	-- Nazan's Riding Crop (QI!)
+				}),
+				o(185168, {	-- Reinforced Fel Iron Chest
+					["description"] = "Available after defeating both Vazruden the Herald and Nazan.",
+					["crs"] = { 17307, 17536 },	-- Vazruden and Nazan
+					["groups"] = {
+						i(27453),	-- Averinn's Ring of Slaying
+						i(27461),	-- Chestguard of the Prowler
+						i(29346),	-- Feltooth Eviscerator
+						i(27455),	-- Irondrake Faceguard
+						i(27457),	-- Life Bearer's Gauntlets
+						i(27452),	-- Light Scribe Bands
+						i(29238),	-- Lion's Heart Girdle
+						i(27458),	-- Oceansong Kilt
+						i(27456),	-- Raiments of Nature's Breath
+						i(27460),	-- Reavers' Ring
+						i(29264),	-- Tree-Mender's Belt
+						i(27459),	-- Vambraces of Daring
+						i(27454),	-- Volcanic Pauldrons
+						i(32077),	-- Wrath-Infused Gauntlets
+					},
+				}),
 			}),
+			-- #endif
 		},
 	}),
 }))));
